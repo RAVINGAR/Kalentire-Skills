47a36f6a21e05239f41835175f64dea7af0b2933
Sleaker <sleaker@gmail.com>
2011-10-28 10:32:35 -0700

    drainsoul now properly fires a HeroRegainHealthEvent

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
@@ -5,41 +5,46 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.config.ConfigurationNode;
 
 import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.api.HeroRegainHealthEvent;
 import com.herocraftonline.dev.heroes.hero.Hero;
 import com.herocraftonline.dev.heroes.skill.SkillType;
 import com.herocraftonline.dev.heroes.skill.TargettedSkill;
 
 public class SkillDrainsoul extends TargettedSkill {
 
-    public SkillDrainsoul(Heroes plugin) {
-        super(plugin, "Drainsoul");
-        setDescription("Absorb health from target");
-        setUsage("/skill drainsoul <target>");
-        setArgumentRange(0, 1);
-        setIdentifiers("skill drainsoul");
-        setTypes(SkillType.DARK, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
-    }
+	public SkillDrainsoul(Heroes plugin) {
+		super(plugin, "Drainsoul");
+		setDescription("Absorb health from target");
+		setUsage("/skill drainsoul <target>");
+		setArgumentRange(0, 1);
+		setIdentifiers("skill drainsoul");
+		setTypes(SkillType.DARK, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
+	}
 
-    @Override
-    public ConfigurationNode getDefaultConfig() {
-        ConfigurationNode node = super.getDefaultConfig();
-        node.setProperty("absorb-amount", 4);
-        return node;
-    }
+	@Override
+	public ConfigurationNode getDefaultConfig() {
+		ConfigurationNode node = super.getDefaultConfig();
+		node.setProperty("absorb-amount", 4);
+		return node;
+	}
 
-    @Override
-    public boolean use(Hero hero, LivingEntity target, String[] args) {
-        Player player = hero.getPlayer();
+	@Override
+	public boolean use(Hero hero, LivingEntity target, String[] args) {
+		Player player = hero.getPlayer();
 
-        int absorbAmount = getSetting(hero, "absorb-amount", 4, false);
+		int absorbAmount = getSetting(hero, "absorb-amount", 4, false);
 
-        hero.setHealth(hero.getHealth() + absorbAmount);
-        hero.syncHealth();
-        addSpellTarget(target, hero);
-        target.damage(absorbAmount, player);
+		HeroRegainHealthEvent hrEvent = new HeroRegainHealthEvent(hero, absorbAmount, this);
+		plugin.getServer().getPluginManager().callEvent(hrEvent);
+		if (!hrEvent.isCancelled()) {
+			hero.setHealth(hero.getHealth() + hrEvent.getAmount());
+			hero.syncHealth();
+		}
+		addSpellTarget(target, hero);
+		target.damage(absorbAmount, player);
 
-        broadcastExecuteText(hero, target);
-        return true;
-    }
+		broadcastExecuteText(hero, target);
+		return true;
+	}
 
 }
