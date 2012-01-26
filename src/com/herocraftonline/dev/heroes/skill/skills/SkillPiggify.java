59daee16782532fa92e4d051acb905a8113c8d0c
Sleaker <sleaker@comcast.net>
2012-01-25 17:56:27 -0800

    formatting + enchant adjustments

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillPiggify.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillPiggify.java
@@ -60,8 +60,9 @@ public class SkillPiggify extends TargettedSkill {
         PigEffect pEffect = new PigEffect(this, duration, (Creature) creature);
         if (target instanceof Player) {
             plugin.getHeroManager().getHero((Player) target).addEffect(pEffect);
-        } else
+        } else {
             plugin.getEffectManager().addEntityEffect(target, pEffect);
+        }
         
         broadcastExecuteText(hero, target);
         return SkillResult.NORMAL;
