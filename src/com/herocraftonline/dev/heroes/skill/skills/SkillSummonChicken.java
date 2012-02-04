9e0b8b18f14d251c3974ee7708d222de7ae80530
Sleaker <sleaker@comcast.net>
2012-02-04 10:21:17 -0800

    sheep/chicken identifier/name fix

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillSummonChicken.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillSummonChicken.java
@@ -19,11 +19,11 @@ import org.bukkit.entity.CreatureType;
 public class SkillSummonChicken extends ActiveSkill {
 
     public SkillSummonChicken(Heroes plugin) {
-        super(plugin, "Pig");
+        super(plugin, "Chicken");
         setDescription("100% chance to spawn 1 chicken, $1% for 2, and $2% for 3.");
         setUsage("/skill chicken");
         setArgumentRange(0, 0);
-        setIdentifiers("skill summonpig", "skill chicken");
+        setIdentifiers("skill summonchicken", "skill chicken");
         setTypes(SkillType.SUMMON, SkillType.SILENCABLE, SkillType.EARTH);
     }
 
--- src/com/herocraftonline/dev/heroes/skill/skills/SkillSummonSheep.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillSummonSheep.java
@@ -21,11 +21,11 @@ import org.bukkit.entity.CreatureType;
 public class SkillSummonSheep extends ActiveSkill {
 
     public SkillSummonSheep(Heroes plugin) {
-        super(plugin, "Pig");
+        super(plugin, "Sheep");
         setDescription("100% chance to spawn 1 sheep, $1% for 2, and $2% for 3.");
         setUsage("/skill sheep");
         setArgumentRange(0, 0);
-        setIdentifiers("skill summonpig", "skill sheep");
+        setIdentifiers("skill summonsheep", "skill sheep");
         setTypes(SkillType.SUMMON, SkillType.SILENCABLE, SkillType.EARTH);
     }
 
