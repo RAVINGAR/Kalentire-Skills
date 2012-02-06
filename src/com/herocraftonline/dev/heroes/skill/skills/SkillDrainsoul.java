3453728ffcb10083e04b101638103bac0a6cb06f
Sleaker <sleaker@comcast.net>
2012-02-05 21:37:01 -0800

    added damage-per-level node to DrainSoul

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
@@ -29,6 +29,7 @@ public class SkillDrainsoul extends TargettedSkill {
 	public ConfigurationSection getDefaultConfig() {
 		ConfigurationSection node = super.getDefaultConfig();
 		node.set(Setting.DAMAGE.node(), 4);
+		node.set(Setting.DAMAGE_INCREASE.node(), 0.0);
 		return node;
 	}
 
@@ -37,7 +38,8 @@ public class SkillDrainsoul extends TargettedSkill {
 		Player player = hero.getPlayer();
 
 		int absorbAmount = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
-
+		absorbAmount += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
+		
 		HeroRegainHealthEvent hrEvent = new HeroRegainHealthEvent(hero, absorbAmount, this);
 		plugin.getServer().getPluginManager().callEvent(hrEvent);
 		if (!hrEvent.isCancelled()) {
@@ -54,6 +56,7 @@ public class SkillDrainsoul extends TargettedSkill {
     @Override
     public String getDescription(Hero hero) {
         int amount = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
+        amount += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
         return getDescription().replace("$1", amount + "");
     }
 
