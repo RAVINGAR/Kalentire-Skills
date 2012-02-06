90e8268d5d5d6291bb94f91ea2c119920a2bbb57
Sleaker <sleaker@comcast.net>
2012-02-05 21:40:01 -0800

    added heal multiplier to drainsoul so it can be configured to return only a percentage of the amount of damage being dealt

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillDrainsoul.java
@@ -18,7 +18,7 @@ public class SkillDrainsoul extends TargettedSkill {
 
 	public SkillDrainsoul(Heroes plugin) {
 		super(plugin, "Drainsoul");
-		setDescription("You drain $1 health from target, restoring your own for the same amount.");
+		setDescription("You drain $1 health from target, restoring $2 of your own health.");
 		setUsage("/skill drainsoul <target>");
 		setArgumentRange(0, 1);
 		setIdentifiers("skill drainsoul");
@@ -30,17 +30,18 @@ public class SkillDrainsoul extends TargettedSkill {
 		ConfigurationSection node = super.getDefaultConfig();
 		node.set(Setting.DAMAGE.node(), 4);
 		node.set(Setting.DAMAGE_INCREASE.node(), 0.0);
+		node.set("heal-mult", .5);
 		return node;
 	}
 
 	@Override
 	public SkillResult use(Hero hero, LivingEntity target, String[] args) {
 		Player player = hero.getPlayer();
-
+		
 		int absorbAmount = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
 		absorbAmount += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
-		
-		HeroRegainHealthEvent hrEvent = new HeroRegainHealthEvent(hero, absorbAmount, this);
+		double healMult = SkillConfigManager.getUseSetting(hero, this, "heal-mult", .5, false);
+		HeroRegainHealthEvent hrEvent = new HeroRegainHealthEvent(hero, (int) (absorbAmount * healMult), this);
 		plugin.getServer().getPluginManager().callEvent(hrEvent);
 		if (!hrEvent.isCancelled()) {
 			hero.setHealth(hero.getHealth() + hrEvent.getAmount());
@@ -57,7 +58,8 @@ public class SkillDrainsoul extends TargettedSkill {
     public String getDescription(Hero hero) {
         int amount = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
         amount += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
-        return getDescription().replace("$1", amount + "");
+        double mult = SkillConfigManager.getUseSetting(hero, this, "heal-mult", .5, false);
+        return getDescription().replace("$1", amount + "").replace("$2", (int) (mult * amount) + "");
     }
 
 }
