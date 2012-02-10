bd0dd9156903f74358fb84fef8e06071fa3ad5f1
Sleaker <sleaker@comcast.net>
2012-02-10 06:58:49 -0800

    layhands is now affected by healing reduction.

--- src/com/herocraftonline/dev/heroes/skill/skills/SkillLayhands.java
+++ src/com/herocraftonline/dev/heroes/skill/skills/SkillLayhands.java
@@ -4,10 +4,12 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import com.herocraftonline.dev.heroes.Heroes;
+import com.herocraftonline.dev.heroes.api.HeroRegainHealthEvent;
 import com.herocraftonline.dev.heroes.api.SkillResult;
 import com.herocraftonline.dev.heroes.hero.Hero;
 import com.herocraftonline.dev.heroes.skill.SkillType;
 import com.herocraftonline.dev.heroes.skill.TargettedSkill;
+import com.herocraftonline.dev.heroes.util.Messaging;
 
 public class SkillLayhands extends TargettedSkill {
 
@@ -27,8 +29,15 @@ public class SkillLayhands extends TargettedSkill {
         }
 
         Hero targetHero = plugin.getHeroManager().getHero((Player) target);
-
-        targetHero.setHealth(targetHero.getMaxHealth());
+        double currentHealth = hero.getHealth();
+        int healAmount = (int) Math.ceil(hero.getMaxHealth() - currentHealth);
+        HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(targetHero, healAmount, this);
+        plugin.getServer().getPluginManager().callEvent(hrhEvent);
+        if (hrhEvent.isCancelled()) {
+            Messaging.send(hero.getPlayer(), "Unable to heal the target at this time!");
+            return SkillResult.CANCELLED;
+        }
+        targetHero.setHealth(currentHealth + hrhEvent.getAmount());
         targetHero.syncHealth();
 
         broadcastExecuteText(hero, target);
