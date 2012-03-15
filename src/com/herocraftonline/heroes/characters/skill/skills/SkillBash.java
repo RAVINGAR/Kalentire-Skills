468801649edc082cfafbdfa0f331e0dd69c5797d
Sleaker <sleaker@gmail.com>
2012-03-14 22:11:35 -0700

    added bash - deals damage and interrupts the target

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillBash.java
@@ -0,0 +1,51 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.util.Setting;
+
+public class SkillBash extends TargettedSkill {
+
+    public SkillBash(Heroes plugin) {
+        super(plugin, "Bash");
+        setDescription("You deal $1 physical damage to your target and interrupt any spell they are currently using.");
+        setUsage("/skill bash <target>");
+        setArgumentRange(0, 1);
+        setIdentifiers("skill bash");
+        setTypes(SkillType.PHYSICAL, SkillType.HARMFUL, SkillType.INTERRUPT);
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+        node.set(Setting.DAMAGE.node(), 10);
+        node.set(Setting.DAMAGE_INCREASE.node(), 0.0);
+        return node;
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
+        damage += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
+        if (!damageEntity(target, hero.getPlayer(), damage, DamageCause.ENTITY_ATTACK)) {
+            return SkillResult.INVALID_TARGET;
+        }
+        broadcastExecuteText(hero, target);
+        return SkillResult.NORMAL;
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 10, false);
+        damage += SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this);
+        return getDescription().replace("$1", damage + "");
+    }
+}
