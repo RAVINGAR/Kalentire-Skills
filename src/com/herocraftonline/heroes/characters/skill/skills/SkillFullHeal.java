52fa1e06fc9dfaa9af9b57e79eaa612bf86e687e
Kainzo <LordKainzo@hotmail.com>
2012-03-18 01:47:04 -0500

    Cloned Layhands to make for a FullHeal for Clerical types.

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillFullHeal.java
@@ -0,0 +1,50 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.util.Messaging;
+
+public class SkillFullHeal extends TargettedSkill {
+
+    public SkillFullHeal(Heroes plugin) {
+        super(plugin, "FullHeal");
+        setDescription("You restore your target to full health.");
+        setUsage("/skill fullheal <target>");
+        setArgumentRange(0, 1);
+        setIdentifiers("skill fullheal");
+        setTypes(SkillType.LIGHT, SkillType.HEAL, SkillType.SILENCABLE);
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        if (!(target instanceof Player)) {
+            return SkillResult.INVALID_TARGET;
+        }
+
+        Hero targetHero = plugin.getCharacterManager().getHero((Player) target);
+        int healAmount = (int) Math.ceil(targetHero.getMaxHealth() - targetHero.getHealth());
+        HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(targetHero, healAmount, this);
+        plugin.getServer().getPluginManager().callEvent(hrhEvent);
+        if (hrhEvent.isCancelled()) {
+            Messaging.send(hero.getPlayer(), "Unable to heal the target at this time!");
+            return SkillResult.CANCELLED;
+        }
+        targetHero.setHealth(targetHero.getHealth() + hrhEvent.getAmount());
+        targetHero.syncHealth();
+
+        broadcastExecuteText(hero, target);
+        return SkillResult.NORMAL;
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        return getDescription();
+    }
+}
