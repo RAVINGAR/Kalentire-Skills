616bf865e8108b18c9d6b77c60b6c1d064f73a46
Kainzo <LordKainzo@hotmail.com>
2012-11-09 09:14:26 -0600

    Attempt to fix MelodicBinding slow duration, changed SkillType from Beneficial

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillMelodicBinding.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillMelodicBinding.java
@@ -45,7 +45,7 @@ public class SkillMelodicBinding extends ActiveSkill {
         setUsage("/skill melodicbinding");
         setArgumentRange(0, 0);
         setIdentifiers("skill melodicbinding");
-        setTypes(SkillType.BUFF, SkillType.SILENCABLE, SkillType.ICE);
+        setTypes(SkillType.HARMFUL, SkillType.SILENCABLE, SkillType.MOVEMENT);
         Bukkit.getServer().getPluginManager().registerEvents(new SkillBlockListener(), plugin);
     }
 
