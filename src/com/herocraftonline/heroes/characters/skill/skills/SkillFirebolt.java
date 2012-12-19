e06f1577f5dfeea42c81bbe01e7ca758e59d19ca
Kainzo <LordKainzo@hotmail.com>
2012-12-19 09:46:01 -0600

    Change up Firebolt to see if Fiery death happens in PVP-OFF.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillFirebolt.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillFirebolt.java
@@ -2,19 +2,18 @@ package com.herocraftonline.heroes.characters.skill.skills;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.SmallFireball;
+import org.bukkit.entity.LargeFireball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 
 import com.herocraftonline.heroes.Heroes;
@@ -28,89 +27,115 @@ import com.herocraftonline.heroes.util.Setting;
 
 public class SkillFirebolt extends ActiveSkill {
 
-    private Map<SmallFireball, Long> fireballs = new LinkedHashMap<SmallFireball, Long>(100) {
-        private static final long serialVersionUID = 4329526013158603250L;
-        @Override
-        protected boolean removeEldestEntry(Entry<SmallFireball, Long> eldest) {
-            return (size() > 60 || eldest.getValue() + 5000 <= System.currentTimeMillis());
-        }
-    };
-    
-    public SkillFirebolt(Heroes plugin) {
-        super(plugin, "Firebolt");
-        setDescription("You shoot a fireball that deals $1 damage!");
-        setUsage("/skill firebolt");
-        setArgumentRange(0, 0);
-        setIdentifiers("skill firebolt");
-        setTypes(SkillType.FIRE, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
-        Bukkit.getServer().getPluginManager().registerEvents(new SkillEntityListener(this), plugin);
-    }
 
-    @Override
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection node = super.getDefaultConfig();
-        node.set(Setting.DAMAGE.node(), 4);
-        node.set(Setting.DAMAGE_INCREASE.node(), 0.0);
-        node.set("velocity-multiplier", 1.5);
-        return node;
-    }
-  
-    @Override
-    public SkillResult use(Hero hero, String[] args) {
-        Player player = hero.getPlayer();
-        SmallFireball fireball = player.launchProjectile(SmallFireball.class);
-        fireballs.put(fireball, System.currentTimeMillis());
-        double mult = SkillConfigManager.getUseSetting(hero, this, "velocity-multiplier", 1.5, false);
-        fireball.setVelocity(fireball.getVelocity().multiply(mult));
-        fireball.setShooter(player);
-        broadcastExecuteText(hero);
-    	fireball.setIsIncendiary(false);
-        return SkillResult.NORMAL;
-    }
+	private Map<LargeFireball, FireballData> fireballs = new LinkedHashMap<LargeFireball, FireballData>(100) {
+       private static final long serialVersionUID = 4329526013158603250L;
+   };
+   private class FireballData {
+   	long creationtime;
+   	Player player;
+   	FireballData(long creationtime, Player player) {
+   		this.creationtime = creationtime;
+   		this.player = player;
+   	}
+   }
+   
+   public SkillFirebolt(Heroes plugin) {
+       super(plugin, "Firebolt");
+       setDescription("You shoot a fireball that deals $1 damage!");
+       setUsage("/skill firebolt");
+       setArgumentRange(0, 0);
+       setIdentifiers("skill firebolt");
+       setTypes(SkillType.FIRE, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
+       Bukkit.getServer().getPluginManager().registerEvents(new SkillEntityListener(this), plugin);
+   }
 
-    public class SkillEntityListener implements Listener {
+   @Override
+   public ConfigurationSection getDefaultConfig() {
+       ConfigurationSection node = super.getDefaultConfig();
+       node.set(Setting.DAMAGE.node(), 4);
+       node.set(Setting.DAMAGE_INCREASE.node(), 0.0);
+       node.set("velocity-multiplier", 0.5);
+       return node;
+   }
 
-        private final Skill skill;
+   @Override
+   public SkillResult use(Hero hero, String[] args) {
+       Player player = hero.getPlayer();
+       LargeFireball fireball = player.launchProjectile(LargeFireball.class);
+       fireball.setIsIncendiary(false);
+       fireball.setYield(0F);
+       fireballs.put(fireball, new FireballData(System.currentTimeMillis(),player));
+       double mult = SkillConfigManager.getUseSetting(hero, this, "velocity-multiplier", 1.5, false);
+       fireball.setVelocity(fireball.getVelocity().multiply(mult));
+       fireball.setShooter(player);
+       broadcastExecuteText(hero); 
+       return SkillResult.NORMAL;
+   }
 
-        public SkillEntityListener(Skill skill) {
-            this.skill = skill;
-        }
+   public class SkillEntityListener implements Listener {
 
-        @EventHandler()
-        public void onEntityDamage(EntityDamageEvent event) {
-            if (event.isCancelled() || !(event instanceof EntityDamageByEntityEvent) || !(event.getEntity() instanceof LivingEntity)) {
-                return;
-            }
+       private final Skill skill;
 
-            EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
-            Entity fireball = subEvent.getDamager();
-            if (!(fireball instanceof SmallFireball) || !fireballs.containsKey(fireball)) {
-                return;
-            }
-            fireballs.remove(fireball);
-            LivingEntity entity = (LivingEntity) subEvent.getEntity();
-            Entity dmger = ((SmallFireball) fireball).getShooter();
-            if (dmger instanceof Player) {
-                Hero hero = plugin.getCharacterManager().getHero((Player) dmger);
+       public SkillEntityListener(Skill skill) {
+           this.skill = skill;
+       }
 
-                if (!damageCheck((Player) dmger, entity)) {
-                    event.setCancelled(true);
-                    return;
-                }
+       @EventHandler()
+       public void onEntityDamage(EntityDamageEvent event) {
+           if (event.isCancelled() || !(event instanceof EntityDamageByEntityEvent) || !(event.getEntity() instanceof LivingEntity)) {
+               return;
+           }
 
-                addSpellTarget(entity, hero);
-                int damage = SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE, 4, false);
-                damage += (int) (SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(skill));
-                damageEntity(entity, hero.getPlayer(), damage, EntityDamageEvent.DamageCause.MAGIC);
-                event.setCancelled(true);
-            }
-        }
-    }
+           EntityDamageByEntityEvent subEvent = (EntityDamageByEntityEvent) event;
+           Entity fireball = subEvent.getDamager();
+           if (!(fireball instanceof LargeFireball) || !fireballs.containsKey(fireball)) {
+               return;
+           }
+           Player dmger = fireballs.get(fireball).player;
+           fireballs.remove(fireball);
+           LivingEntity entity = (LivingEntity) subEvent.getEntity();
+           Hero hero = plugin.getCharacterManager().getHero(dmger);
+           event.setCancelled(true);
+           if(!Skill.damageCheck(dmger, entity)) {
+           	return;
+           }
+           addSpellTarget(entity, hero);
+           int damage = SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE, 4, false);
+           damage += (int) (SkillConfigManager.getUseSetting(hero, skill, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(skill));
+           damageEntity(entity, hero.getPlayer(), damage, EntityDamageEvent.DamageCause.MAGIC);
+       }
+       
+       @EventHandler(priority = EventPriority.LOWEST)
+       public void onGhastProjectileHit(EntityExplodeEvent event) {
+       	if(event.isCancelled() || !(event.getEntity() instanceof LargeFireball)) {
+       		return;
+       	}
+       	LargeFireball fireball = (LargeFireball) event.getEntity();
+       	if(fireballs.containsKey(fireball)) {
+       		fireballs.remove(fireball);
+       		event.setCancelled(true);
+       	}
+       }
+       @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+       public void onProjectileHit(ProjectileHitEvent event) {
+       	if(!(event.getEntity() instanceof LargeFireball)) {
+       		return;
+       	}
+       	LargeFireball fireball = (LargeFireball)event.getEntity();
+       	if(!fireballs.containsKey(fireball)) {
+       		return;
+       	}
+       	fireballs.remove(fireball);
+       	fireball.setIsIncendiary(false);
+       	fireball.setFireTicks(0);
+       }
+   }
 
-    @Override
-    public String getDescription(Hero hero) {
-        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 1, false);
-        damage += (int) (SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this));
-        return getDescription().replace("$1", damage + "");
-    }
+   @Override
+   public String getDescription(Hero hero) {
+       int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 1, false);
+       damage += (int) (SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0.0, false) * hero.getSkillLevel(this));
+       return getDescription().replace("$1", damage + "");
+   }
 }
