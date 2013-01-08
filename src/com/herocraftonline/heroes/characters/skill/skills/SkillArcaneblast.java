5a9c56b887e9127732f2deb2c55ba11293b16443
Kainzo <LordKainzo@hotmail.com>
2013-01-07 23:29:18 -0600

    Implement a broken skill - Arcaneblast (Smite + Fireworks)

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillArcaneblast.java
@@ -0,0 +1,64 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.util.Setting;
+
+public class SkillArcaneblast extends TargettedSkill {
+	
+
+    public SkillArcaneblast(Heroes plugin) {
+        super(plugin, "Arcaneblast");
+        setDescription("You arcaneblast the target for $1 light damage.");
+        setUsage("/skill arcaneblast");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill arcaneblast");
+        setTypes(SkillType.DAMAGING, SkillType.LIGHT, SkillType.SILENCABLE, SkillType.HARMFUL);
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+        node.set(Setting.DAMAGE.node(), 10);
+        return node;
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        Player player = hero.getPlayer();
+
+        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 10, false);
+        addSpellTarget(target, hero);
+        int n = 5; //Replace me with config values for number of fireworks spawned
+		for(int i = 0;i<n;i++)
+        damageEntity(target, player, damage, DamageCause.MAGIC);
+        broadcastExecuteText(hero, target);
+		Firework firework = (Firework) event.getHero().getPlayer().getWorld().spawnEntity(loc, EntityType.FIREWORK);
+		FireworkMeta meta = firework.setFireworkMeta();
+		meta.addEffect(FireworkEffect.builder().flicker(false).trail(true).with(Type.BALL_LARGE).withColor(Color.BLUE).withFade(Color.WHITE).build());
+        return SkillResult.NORMAL;
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 10, false);
+        return getDescription().replace("$1", damage + "");
+    }
+
+}
