cf09635f18881b5903c5fbf26329a180540e459f
Kainzo <LordKainzo@hotmail.com>
2013-01-09 20:29:41 -0600

    Arcaneblast fixes

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillArcaneblast.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillArcaneblast.java
@@ -1,10 +1,5 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
@@ -12,44 +7,59 @@ import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.characters.skill.TargettedSkill;
 import com.herocraftonline.heroes.util.Setting;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Builder;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.inventory.meta.FireworkMeta;
 
-public class SkillArcaneblast extends TargettedSkill {
+public class SkillArcaneblast extends TargettedSkill
+{
+  public SkillArcaneblast(Heroes plugin)
+  {
+    super(plugin, "Arcaneblast");
+    setDescription("You arcaneblast the target for $1 light damage.");
+    setUsage("/skill arcaneblast");
+    setArgumentRange(0, 0);
+    setIdentifiers(new String[] { "skill arcaneblast" });
+    setTypes(new SkillType[] { SkillType.DAMAGING, SkillType.LIGHT, SkillType.SILENCABLE, SkillType.HARMFUL });
+  }
 
-    public SkillArcaneblast(Heroes plugin) {
-        super(plugin, "Bolt");
-        setDescription("Calls a bolt of lightning down on the target dealing $1 damage.");
-        setUsage("/skill bolt <target>");
-        setArgumentRange(0, 1);
-        setIdentifiers("skill bolt");
-        setTypes(SkillType.LIGHTNING, SkillType.SILENCABLE, SkillType.DAMAGING, SkillType.HARMFUL);
-    }
+  public ConfigurationSection getDefaultConfig()
+  {
+    ConfigurationSection node = super.getDefaultConfig();
+    node.set(Setting.DAMAGE.node(), Integer.valueOf(10));
+    return node;
+  }
 
-    @Override
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection node = super.getDefaultConfig();
-        node.set(Setting.DAMAGE.node(), 4);
-        node.set(Setting.DAMAGE_INCREASE.node(), 0);
-        return node;
-    }
+  public SkillResult use(Hero hero, LivingEntity target, String[] args)
+  {
+    Player player = hero.getPlayer();
 
-    @Override
-    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
-        Player player = hero.getPlayer();
+    int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 10, false);
+    addSpellTarget(target, hero);
+    int n = 5;
 
-        target.getWorld().strikeLightningEffect(target.getLocation());
-        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
-        damage += (int) (SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0, false) * hero.getSkillLevel(this));
-        plugin.getDamageManager().addSpellTarget(target, hero, this);
-        damageEntity(target, player, damage, DamageCause.MAGIC);
+    damageEntity(target, player, damage, EntityDamageEvent.DamageCause.MAGIC);
+    broadcastExecuteText(hero, target);
+    Firework firework = (Firework)target.getWorld().spawnEntity(target.getLocation(), EntityType.FIREWORK);
+    FireworkMeta meta = firework.getFireworkMeta();
+    meta.addEffect(FireworkEffect.builder().flicker(false).trail(true).with(FireworkEffect.Type.BALL_LARGE).withColor(Color.BLUE).withFade(Color.WHITE).build());
+    meta.setPower(0);
+    firework.setFireworkMeta(meta);
+    return SkillResult.NORMAL;
+  }
 
-        broadcastExecuteText(hero, target);
-        return SkillResult.NORMAL;
-    }
-
-    @Override
-    public String getDescription(Hero hero) {
-        int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 4, false);
-        damage += (int) (SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE_INCREASE, 0, false) * hero.getSkillLevel(this));
-        return getDescription().replace("$1", damage + "");
-    }
+  public String getDescription(Hero hero)
+  {
+    int damage = SkillConfigManager.getUseSetting(hero, this, Setting.DAMAGE, 10, false);
+    return getDescription().replace("$1", damage + "");
+  }
 }
