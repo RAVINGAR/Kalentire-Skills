919bcb2b4986a569a880012ba739c311d5ec5c0e
Kainzo <LordKainzo@hotmail.com>
2013-01-29 23:10:59 -0600

    Lets try to fix Disenchant.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillDisenchant.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDisenchant.java
@@ -1,56 +1,65 @@
 package com.herocraftonline.heroes.characters.skill.skills;
+//src=http://pastie.org/private/qi5088ssrpskghzs3tujg#31
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Random;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
-import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Messaging;
+import com.herocraftonline.heroes.util.Util;
 
 public class SkillDisenchant extends ActiveSkill {
 
-    public SkillDisenchant(Heroes plugin) {
-        super(plugin, "Disenchant");
-        setDescription("You are able to disenchant items, returning them to normal.");
-        setArgumentRange(0, 0);
-        setTypes(SkillType.KNOWLEDGE, SkillType.ITEM);
-        setIdentifiers("skill disenchant", "skill disench");
-    }
+	public SkillDisenchant(Heroes plugin) {
+		super(plugin, "Disenchant");
+		setDescription("You are able to disenchant items, returning them to normal.");
+		setIdentifiers(new String[] { "skill disenchant" });
+		setUsage("/skill disenchant");
+		setArgumentRange(0,0);
+	}
 
-    @SuppressWarnings("deprecation")
-    @Override
-    public SkillResult use(Hero hero, String[] args) {
-        Player player = hero.getPlayer();
-        ItemStack item = player.getItemInHand();
-        if (item == null || item.getType() == Material.AIR) {
-            Messaging.send(player, "You must have an item to disenchant!");
-            return SkillResult.INVALID_TARGET_NO_MSG;
-        }
-        List<Enchantment> enchants = new ArrayList<Enchantment>(item.getItemMeta().getEnchants().keySet());
-        if (enchants.isEmpty()) {
-            Messaging.send(player, "That item has no enchantments!");
-            return SkillResult.INVALID_TARGET_NO_MSG;
-        }
-        
-        for (Enchantment enchant : item.getItemMeta().getEnchants().keySet()) {
-            item.getItemMeta().removeEnchant(enchant);
-        }
-        player.updateInventory();
-        broadcastExecuteText(hero);
-        return SkillResult.NORMAL;
-    }
+	@SuppressWarnings("deprecation")
+	@Override
+	public SkillResult use(Hero h, String[] args) {
+		ItemStack hand = h.getPlayer().getItemInHand();
+		if(!(Util.isArmor(hand.getType()) || Util.isWeapon(hand.getType()))) {
+			h.getPlayer().sendMessage(ChatColor.GRAY + "This is not a disenchantable item!");
+			return SkillResult.INVALID_TARGET_NO_MSG;
+		}
+		Random randgen = new Random();
+		//Get all enchants currently on an item
+		Iterator<Entry<Enchantment, Integer>> enchants = hand.getEnchantments().entrySet().iterator();
+		//Go through each of these enchants 1 by 1
+		while(enchants.hasNext()) {
+			Entry<Enchantment, Integer> next = enchants.next();
+			Enchantment ench = next.getKey();
+			//Skip over sharpness/prot/punch because  that is durability dependent, not enchantment dependent
+			if(ench.equals(Enchantment.ARROW_DAMAGE) || ench.equals(Enchantment.DAMAGE_ALL) || ench.equals(Enchantment.PROTECTION_ENVIRONMENTAL)) {
+				continue;
+			}
+			hand.removeEnchantment(ench);
+			//Handle breaking
+			if(randgen.nextInt(100) < Math.pow(h.getLevel(h.getSecondClass()),-1)*100) {
+				h.getPlayer().sendMessage(ChatColor.GRAY + "Oh no your item broke :("); //Like we actually give a fuck
+				hand.setAmount(0);
+				h.getPlayer().updateInventory();
+				break;
+			}
+		}
+		return SkillResult.NORMAL;
+	}
 
-    @Override
-    public String getDescription(Hero hero) {
-        return getDescription();
-    }
+	@Override
+	public String getDescription(Hero h) {
+		return getDescription().replace("$1", Math.pow(h.getLevel(h.getSecondClass()),-1)*100 + "");
+	}
+
 
 }
