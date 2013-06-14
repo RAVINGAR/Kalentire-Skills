48cf2b0838a89d0c8aac85918bba9cd91bc76033
Kainzo <LordKainzo@hotmail.com>
2013-06-14 01:37:34 -0500

    Fix up stuff.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillInfusion.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillInfusion.java
@@ -1,7 +1,7 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -10,80 +10,116 @@ import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.BloodUnionEffect;
 import com.herocraftonline.heroes.characters.effects.Effect;
 import com.herocraftonline.heroes.characters.effects.EffectType;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.characters.skill.VisualEffect;
 import com.herocraftonline.heroes.util.Messaging;
 
 public class SkillInfusion extends TargettedSkill {
+	public VisualEffect fplayer = new VisualEffect();
 
-    public SkillInfusion(Heroes plugin) {
-        super(plugin, "Infusion");
-        setDescription("Infuses your target, restoring $1 health and negates bleeding.");
-        setUsage("/skill infusion <target>");
-        setArgumentRange(0, 1);
-        setIdentifiers("skill infusion");
-        setTypes(SkillType.HEAL, SkillType.DARK, SkillType.SILENCABLE);
-    }
+	public SkillInfusion(Heroes plugin) {
+		super(plugin, "Infusion");
+		setDescription("Infuse your target with life, restoring $1 of their health and negating their bleeding. Healing is improved by $2% per level of Blood Union. This ability costs $3 health and $4 mana to use.");
+		setUsage("/skill infusion <target>");
+		setArgumentRange(0, 1);
+		setIdentifiers("skill infusion");
+		setTypes(SkillType.HEAL, SkillType.SILENCABLE, SkillType.LIGHT);
+	}
 
-    @Override
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection section = super.getDefaultConfig();
-        section.set(SkillSetting.HEALTH.node(), 5);
-        section.set(SkillSetting.HEALTH_INCREASE.node(), 0);
-        section.set(SkillSetting.MAX_DISTANCE.node(), 5);
-        section.set(SkillSetting.REAGENT.node(), 339);
-        section.set(SkillSetting.REAGENT_COST.node(), 1);
-        return section;
-    }
+	public String getDescription(Hero hero) {
 
-    @Override
-    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
-        Player player = hero.getPlayer();
-        if (!(target instanceof Player)) {
-            return SkillResult.INVALID_TARGET;
-        }
+		int health = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH.node(), 115, false);
+		int healthcost = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH_COST.node(), 50, false);
+		int manacost = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MANA.node(), 10, false);
+		int healIncrease = (int) (SkillConfigManager.getUseSetting(hero, this, "health-increase-percent-per-blood-union", 0.02, false) * 100);
 
-        Hero targetHero = plugin.getCharacterManager().getHero((Player) target);
-        int hpPlus = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH, 5, false);
-        hpPlus += (SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH_INCREASE, 0, false) * hero.getSkillLevel(this));
-        int targetHealth = target.getHealth();
+		return getDescription().replace("$1", health + "").replace("$2", healIncrease + "").replace("$3", healthcost + "").replace("$4", manacost + "");
+	}
 
-        if (targetHealth >= target.getMaxHealth()) {
-            if (player.equals(targetHero.getPlayer())) {
-                Messaging.send(player, "You are already at full health.");
-            } else {
-                Messaging.send(player, "Target is already fully healed.");
-            }
-            return SkillResult.INVALID_TARGET_NO_MSG;
-        }
+	public ConfigurationSection getDefaultConfig() {
 
-        HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(targetHero, hpPlus, this, hero);
-        Bukkit.getPluginManager().callEvent(hrhEvent);
-        if (hrhEvent.isCancelled()) {
-            Messaging.send(player, "Unable to heal the target at this time!");
-            return SkillResult.CANCELLED;
-        }
-        targetHero.heal(hrhEvent.getAmount()); 
+		ConfigurationSection node = super.getDefaultConfig();
 
-        // Infusion cures Bleeding!
-        for (Effect effect : targetHero.getEffects()) {
-            if (effect.isType(EffectType.BLEED)) {
-                targetHero.removeEffect(effect);
-            }
-        }
-        hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.FUSE , 0.5F, 0.01F); 
-        broadcastExecuteText(hero, target);
-        return SkillResult.NORMAL;
-    }
+		node.set("health-increase-percent-per-blood-union", 0.02);
+		node.set(SkillSetting.HEALTH.node(), 10);
+		node.set(SkillSetting.MANA.node(), 25);
 
-    @Override
-    public String getDescription(Hero hero) {
-        double amount = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH, 5, false);
-        amount += (SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH_INCREASE, 0, false) * hero.getSkillLevel(this));
-        return getDescription().replace("$1", amount + "");
-    }
+		return node;
+	}
+
+	public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+		Player player = hero.getPlayer();
+		if (!(target instanceof Player)) {
+			return SkillResult.INVALID_TARGET;
+		}
+
+		Hero targetHero = this.plugin.getCharacterManager().getHero((Player) target);
+
+		int targetHealth = target.getHealth();
+
+		// Check to see if they are at full health
+		if (targetHealth >= target.getMaxHealth()) {
+			if (player.equals(targetHero.getPlayer()))
+				Messaging.send(player, "You are already at full health.", new Object[0]);
+			else {
+				Messaging.send(player, "Target is already at full health.", new Object[0]);
+			}
+
+			return SkillResult.INVALID_TARGET_NO_MSG;
+		}
+
+		int healAmount = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALTH, 10, false);
+
+		// Get Blood Union Level
+		int bloodUnionLevel = 0;
+		if (hero.hasEffect("BloodUnionEffect")) {
+			BloodUnionEffect buEffect = (BloodUnionEffect) hero.getEffect("BloodUnionEffect");
+
+			bloodUnionLevel = buEffect.getBloodUnionLevel();
+		}
+
+		// Increase healing based on blood union level
+		double healIncrease = SkillConfigManager.getUseSetting(hero, this, "health-increase-percent-per-blood-union", 0.02, false);
+		healIncrease = 1 + (healIncrease *= bloodUnionLevel);
+		healAmount *= healIncrease;
+
+		// Ensure they can be healed.
+		HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(targetHero, healAmount, this, hero);
+		this.plugin.getServer().getPluginManager().callEvent(hrhEvent);
+		if (hrhEvent.isCancelled()) {
+			Messaging.send(player, "Unable to heal your target at this time!", new Object[0]);
+			return SkillResult.CANCELLED;
+		}
+
+		broadcastExecuteText(hero, target);
+
+		// Heal target
+		targetHero.heal(hrhEvent.getAmount());
+
+		// Remove bleeds
+		for (Effect effect : targetHero.getEffects()) {
+			if (effect.isType(EffectType.BLEED)) {
+				targetHero.removeEffect(effect);
+			}
+		}
+
+		// Play effect
+		try {
+			this.fplayer.playFirework(player.getWorld(), target.getLocation().add(0.0D, 1.5D, 0.0D), FireworkEffect.builder().flicker(false).trail(false).with(FireworkEffect.Type.BURST).withColor(Color.MAROON).withFade(Color.WHITE).build());
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return SkillResult.NORMAL;
+	}
 }
--- src/com/herocraftonline/heroes/characters/skill/skills/SkillQuiveringPalm.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillQuiveringPalm.java
@@ -29,16 +29,6 @@ import com.herocraftonline.heroes.util.Messaging;
 
 public class SkillQuiveringPalm extends TargettedSkill {
 
-	// Default skill values
-	private final int defDamage = 50;
-	private final double defDamageMultiplier = 1.2;
-	private final int defDuration = 2500;
-
-	private final String skillText = "§7[§2Skill§7] ";		// Used to add "[Skill]" text to all skill related messages
-
-	private final String defApplyText = skillText + "%target% is weakened by a §lQuiveringPalm!";
-	private final String defExpireText = skillText + "%target% has recovered from the effects of the §lQuiveringPalm!";
-
 	public SkillQuiveringPalm(Heroes plugin) {
 		super(plugin, "QuiveringPalm");
 		setDescription("Strike your target with a Quivering Palm dealing $1 damage and weakening the target, causing them to take $2% increased melee damage for $3 seconds.");
@@ -46,28 +36,28 @@ public class SkillQuiveringPalm extends TargettedSkill {
 		setArgumentRange(0, 0);
 		setIdentifiers("skill quiveringpalm");
 		setTypes(SkillType.PHYSICAL, SkillType.DAMAGING, SkillType.HARMFUL, SkillType.DEBUFF);
-		
-		Bukkit.getServer().getPluginManager().registerEvents(new QuiveringPalmListener(this), plugin);
+
+		Bukkit.getServer().getPluginManager().registerEvents(new QuiveringPalmListener(), plugin);
 	}
 
 	@Override
 	public ConfigurationSection getDefaultConfig() {
 		ConfigurationSection node = super.getDefaultConfig();
 
-		node.set(SkillSetting.DAMAGE.node(), defDamage);
-		node.set(SkillSetting.DURATION.node(), defDuration);
-		node.set(SkillSetting.APPLY_TEXT.node(), defApplyText);
-		node.set(SkillSetting.EXPIRE_TEXT.node(), defExpireText);
-		node.set("damage-multiplier", defDamageMultiplier);
-		//node.set(SkillSetting.MAX_DISTANCE.node(), defMaxDistance);
+		node.set(SkillSetting.DAMAGE.node(), 50);
+		node.set(SkillSetting.DURATION.node(), 2500);
+		node.set(SkillSetting.APPLY_TEXT.node(), "§7[§2Skill§7] %target% is weakened by a §lQuiveringPalm!");
+		node.set(SkillSetting.EXPIRE_TEXT.node(), "§7[§2Skill§7] %target% has recovered from the effects of the §lQuiveringPalm!");
+		node.set("damage-multiplier", 1.2);
 		return node;
 	}
 
 	@Override
 	public String getDescription(Hero hero) {
-		int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, defDamage, false);
-		long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, defDuration, false) / 1000;
-		int damageMultiplier = (int) ((SkillConfigManager.getUseSetting(hero, this, "damage-multiplier", defDamageMultiplier, false) - 1) * 100);
+		int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 50, false);
+		double duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 2500, false) / 1000;
+		double damageMultiplier = (int) ((SkillConfigManager.getUseSetting(hero, this, "damage-multiplier", 1.2, false) - 1) * 100);
+
 		return getDescription().replace("$1", damage + "").replace("$2", damageMultiplier + "").replace("$3", duration + "");
 	}
 
@@ -75,7 +65,7 @@ public class SkillQuiveringPalm extends TargettedSkill {
 	public SkillResult use(Hero hero, LivingEntity target, String[] args) {
 		Player player = hero.getPlayer();
 
-		int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, defDamage, false);
+		int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 50, false);
 
 		// Damage the target
 		addSpellTarget(target, hero);
@@ -84,18 +74,15 @@ public class SkillQuiveringPalm extends TargettedSkill {
 		// Play Sound
 		hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.HURT, 0.8F, 1.0F);
 
-		// Play Effect
-		// CODE HERE
-
 		// Display use Message
 		broadcastExecuteText(hero, target);
 
 		// Prep variables
-		double damageMultiplier = SkillConfigManager.getUseSetting(hero, this, "damage-multiplier", defDamageMultiplier, false);
-		long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, defDuration, false);
+		double damageMultiplier = SkillConfigManager.getUseSetting(hero, this, "damage-multiplier", 1.2, false);
+		long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 2500, false);
 
-		String applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, defApplyText).replace("%target%", "$1");
-		String expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, defExpireText).replace("%target%", "$1");
+		String applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, "§7[§2Skill§7] %target% is weakened by a §lQuiveringPalm!").replace("%target%", "$1");
+		String expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, "§7[§2Skill§7] %target% has recovered from the effects of the §lQuiveringPalm!").replace("%target%", "$1");
 
 		// Add the debuff to the target
 		CharacterTemplate targCT = this.plugin.getCharacterManager().getCharacter(target);
@@ -106,10 +93,8 @@ public class SkillQuiveringPalm extends TargettedSkill {
 	}
 
 	private class QuiveringPalmListener implements Listener {
-		private final Skill skill;
 
-		public QuiveringPalmListener(Skill skill) {
-			this.skill = skill;
+		public QuiveringPalmListener() {
 		}
 
 		// Alter damage dealt by players when they are under the effect of quivering palm
