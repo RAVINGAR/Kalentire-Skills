6114add9a896e8ce7f1053180408833f2d99b0ff
Delfofthebla <Delfofthebla@gmail.com>
2013-08-01 09:34:35 -0700

    * Add a few new skills.

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillArcaneTransfer.java
@@ -0,0 +1,88 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import java.util.ArrayList;
+
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.CharacterTemplate;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.Effect;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.effects.PeriodicDamageEffect;
+import com.herocraftonline.heroes.characters.effects.PeriodicHealEffect;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.util.Messaging;
+import com.herocraftonline.heroes.util.Util;
+
+public class SkillArcaneTransfer extends TargettedSkill {
+
+    public SkillArcaneTransfer(Heroes plugin) {
+        super(plugin, "ArcaneTransfer");
+        setDescription("Transfer up to $1 of your harmful effects to your target.");
+        setUsage("/skill arcanetransfer");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill arcanetransfer");
+        setTypes(SkillType.SILENCABLE, SkillType.HARMFUL);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        int removals = SkillConfigManager.getUseSetting(hero, this, "max-transfers", 2, false);
+        return getDescription().replace("$1", removals + "");
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set("max-transfers", 2);
+
+        return node;
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        Player player = hero.getPlayer();
+
+        ArrayList<Effect> possibleEffects = new ArrayList<Effect>();
+        for (Effect effect : hero.getEffects()) {
+            if (effect.isType(EffectType.HARMFUL) && effect.isType(EffectType.DISPELLABLE)) {
+                possibleEffects.add(effect);
+            }
+        }
+
+        if (possibleEffects.isEmpty()) {
+            Messaging.send(player, "You have no effects to transfer!");
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
+
+        CharacterTemplate targCT = plugin.getCharacterManager().getCharacter((LivingEntity) target);
+        int maxTransfers = SkillConfigManager.getUseSetting(hero, this, "max-transfers", 2, false);
+        for (int i = 0; i < maxTransfers && possibleEffects.size() > 0; i++) {
+            Effect stolenEffect = possibleEffects.get(Util.nextInt(possibleEffects.size()));
+            hero.removeEffect(stolenEffect);
+
+            if (stolenEffect instanceof PeriodicDamageEffect) {
+                ((PeriodicDamageEffect) stolenEffect).setApplierHero(hero);
+            }
+            else if (stolenEffect instanceof PeriodicHealEffect) {
+                ((PeriodicHealEffect) stolenEffect).setApplierHero(hero);
+            }
+
+            targCT.addEffect(stolenEffect);
+            possibleEffects.remove(stolenEffect);
+        }
+
+        player.getWorld().playSound(player.getLocation(), Sound.GHAST_MOAN, 0.8F, 1.0F);
+        broadcastExecuteText(hero, target);
+
+        return SkillResult.NORMAL;
+    }
+}
---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillFlicker.java
@@ -0,0 +1,122 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.Monster;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.effects.PeriodicExpirableEffect;
+import com.herocraftonline.heroes.characters.effects.common.InvisibleEffect;
+import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.Skill;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.util.Util;
+
+public class SkillFlicker extends ActiveSkill {
+
+    private String applyText;
+    private String expireText;
+
+    public SkillFlicker(Heroes plugin) {
+        super(plugin, "Flicker");
+        setDescription("You appear to flicker in and out of sight for $1 seconds.");
+        setUsage("/skill flicker");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill flicker");
+        setTypes(SkillType.ILLUSION, SkillType.BUFF, SkillType.SILENCABLE);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        double duration = Util.formatDouble(SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 10000, false) / 1000.0);
+
+        return getDescription().replace("$1", duration + "");
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set(SkillSetting.DURATION.node(), 10000);
+        node.set(SkillSetting.PERIOD.node(), 1500);
+        node.set("invis-duration", 500);
+        node.set(SkillSetting.APPLY_TEXT.node(), ChatColor.GRAY + "[" + ChatColor.DARK_GREEN + "Skill" + ChatColor.GRAY + "] %hero% begins to flicker!");
+        node.set(SkillSetting.EXPIRE_TEXT.node(), ChatColor.GRAY + "[" + ChatColor.DARK_GREEN + "Skill" + ChatColor.GRAY + "] %hero% is no longer flickering.");
+
+        return node;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+
+        applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, ChatColor.GRAY + "[" + ChatColor.DARK_GREEN + "Skill" + ChatColor.GRAY + "] %hero% begins to flicker!").replace("%hero%", "$1");
+        expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, ChatColor.GRAY + "[" + ChatColor.DARK_GREEN + "Skill" + ChatColor.GRAY + "] %hero% is no longer flickering.").replace("%hero%", "$1");
+    }
+
+    @Override
+    public SkillResult use(Hero hero, String[] args) {
+        broadcastExecuteText(hero);
+
+        long period = SkillConfigManager.getUseSetting(hero, this, SkillSetting.PERIOD, 1500, false);
+        long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 10000, false);
+        long invisDuration = SkillConfigManager.getUseSetting(hero, this, "invis-duration", 500, false);
+        hero.addEffect(new FlickerEffect(this, period, duration, invisDuration));
+
+        return SkillResult.NORMAL;
+    }
+    
+    private class FlickerEffect extends PeriodicExpirableEffect {
+
+        long invisDuration;
+        
+        public FlickerEffect(Skill skill, long period, long duration, long invisDuration) {
+            super(skill, "Flicker", period, duration);
+
+            types.add(EffectType.BENEFICIAL);
+            types.add(EffectType.MAGIC);
+            types.add(EffectType.DISPELLABLE);
+            
+            this.invisDuration = invisDuration;
+        }
+        
+        @Override
+        public void applyToHero(Hero hero) {
+            super.applyToHero(hero);
+
+            Player player = hero.getPlayer();
+
+            broadcast(player.getLocation(), applyText, player.getDisplayName());
+        }
+
+        @Override
+        public void removeFromHero(Hero hero) {
+            super.removeFromHero(hero);
+
+            Player player = hero.getPlayer();
+            broadcast(player.getLocation(), expireText, player.getDisplayName());
+        }
+
+        @Override
+        public void tickHero(Hero hero) {
+            Player player = hero.getPlayer();
+            Location playerLoc = player.getLocation();
+            player.getWorld().playEffect(playerLoc, Effect.ENDER_SIGNAL, 3);
+            player.getWorld().playSound(playerLoc, Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
+
+            hero.addEffect(new InvisibleEffect(skill, invisDuration, null, null));
+        }
+
+        @Override
+        public void tickMonster(Monster monster) {}
+    }
+}
