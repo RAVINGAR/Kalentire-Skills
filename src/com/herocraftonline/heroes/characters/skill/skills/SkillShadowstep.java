6a3472822446128c36c3f78cd47ba780d1c50d6f
Delfofthebla <Delfofthebla@gmail.com>
2013-08-11 21:12:05 -0700

    * Fix an exploit with Shadowstep.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillShadowstep.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillShadowstep.java
@@ -9,6 +9,8 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
@@ -22,97 +24,133 @@ import com.herocraftonline.heroes.util.Util;
 
 public class SkillShadowstep extends TargettedSkill {
 
-	private String noLineOfSightText;
+    //private final BlockFace[] faces = { BlockFace.NORTH, BlockFace.NORTH_EAST, BlockFace.EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH, BlockFace.SOUTH_WEST, BlockFace.WEST, BlockFace.NORTH_WEST };
 
-	private String teleFailText;
-	private String useText;
+    public SkillShadowstep(Heroes plugin) {
+        super(plugin, "Shadowstep");
+        setDescription("You teleport behind the target within $1 blocks.");
+        setUsage("/skill shadowstep");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill shadowstep");
+        setTypes(SkillType.DARK, SkillType.HARMFUL, SkillType.TELEPORT, SkillType.SILENCABLE);
+    }
 
-	private final BlockFace[] faces = { BlockFace.NORTH, BlockFace.NORTH_EAST, BlockFace.EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH, BlockFace.SOUTH_WEST, BlockFace.WEST, BlockFace.NORTH_WEST };
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection defaultConfig = super.getDefaultConfig();
 
-	public SkillShadowstep(Heroes plugin) {
-		super(plugin, "Shadowstep");
-		setDescription("You teleport behind the target within $1 blocks.");
-		setUsage("/skill shadowstep");
-		setArgumentRange(0, 0);
-		setIdentifiers("skill shadowstep");
-		setTypes(SkillType.DARK, SkillType.HARMFUL, SkillType.TELEPORT, SkillType.SILENCABLE);
-	}
+        defaultConfig.set(SkillSetting.COOLDOWN.node(), 30000);
+        defaultConfig.set(SkillSetting.MANA.node(), 55);
+        defaultConfig.set(SkillSetting.STAMINA.node(), 0);
+        defaultConfig.set(SkillSetting.MAX_DISTANCE.node(), 5.0);
+        defaultConfig.set(SkillSetting.MAX_DISTANCE_INCREASE.node(), 0.1);
+        defaultConfig.set("blocks-behind-target", 1);
+        defaultConfig.set(SkillSetting.USE_TEXT.node(), ChatColor.GRAY + "[" + ChatColor.DARK_GREEN + "Skill" + ChatColor.GRAY + "] %hero% ShadowStepped behind %target%!");
 
-	@Override
-	public void init() {
-		super.init();
-		useText = SkillConfigManager.getRaw(this, SkillSetting.USE_TEXT, "%hero% ShadowStepped behind %target%!").replace("%hero%", "$1").replace("%target%", "$2");
-		noLineOfSightText = SkillConfigManager.getRaw(this, "no-line-of-sight-text", ChatColor.GRAY.toString() + "Target is not within your line of sight!");
-		teleFailText = SkillConfigManager.getRaw(this, "teleport-fail-text", ChatColor.GRAY.toString() + "Failed to teleport to target.");
-	}
+        return defaultConfig;
+    }
 
-	@Override
-	public ConfigurationSection getDefaultConfig() {
-		ConfigurationSection defaultConfig = super.getDefaultConfig();
-		defaultConfig.set(SkillSetting.COOLDOWN.node(), 30000);
-		defaultConfig.set(SkillSetting.MANA.node(), 55);
-		defaultConfig.set(SkillSetting.STAMINA.node(), 0);
-		defaultConfig.set(SkillSetting.MAX_DISTANCE.node(), 5.0);
-		defaultConfig.set(SkillSetting.MAX_DISTANCE_INCREASE.node(), 0.1);
-		defaultConfig.set(SkillSetting.USE_TEXT.node(), "%hero% ShadowStepped behind %target%!");
-		defaultConfig.set("no-line-of-sight-text", ChatColor.GRAY.toString() + "Target is not within your line of sight!");
-		defaultConfig.set("teleport-fail-text", ChatColor.GRAY.toString() + "Failed to teleport to target.");
-		return defaultConfig;
-	}
+    @Override
+    public String getDescription(Hero hero) {
+        Double distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 5.0, false) + (SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE_INCREASE, 0.1, false) * hero.getSkillLevel(this));
+        return getDescription().replace("$1", distance.toString());
+    }
 
-	@Override
-	public String getDescription(Hero hero) {
-		Double distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 5.0, false) + (SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE_INCREASE, 0.1, false) * hero.getSkillLevel(this));
-		return getDescription().replace("$1", distance.toString());
-	}
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
 
-	@Override
-	public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        Player player = hero.getPlayer();
 
-		if (target == hero.getPlayer() || !(target instanceof Player)) {
-			return SkillResult.INVALID_TARGET_NO_MSG;
-		}
+        if (target == player || !(target instanceof Player)) {
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
 
-		if (!inLineOfSight(hero.getPlayer(), (Player) target)) {
-			hero.getPlayer().sendMessage(noLineOfSightText);
-			return SkillResult.FAIL;
-		}
+        Location playerLoc = player.getLocation();
+        Location targetLoc = target.getLocation();
 
-		Location targetLoc = target.getLocation();
-		BlockFace targetFace = faces[Math.round(targetLoc.getYaw() / 45f) & 0x7];
-		Block teleBlock = targetLoc.getBlock().getRelative(targetFace);
+        BlockIterator iter = null;
+        try {
+            Vector direction = targetLoc.getDirection().multiply(-1);
+            int blocksBehindTarget = SkillConfigManager.getUseSetting(hero, this, "blocks-behind-target", 1, false);
+            iter = new BlockIterator(target.getWorld(), targetLoc.toVector(), direction, 0, blocksBehindTarget);
+        }
+        catch (IllegalStateException e) {
+            Messaging.send(player, "There was an error getting the Shadowstep location!");
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
 
-		// Ensure that the location isn't inside of a block
-		if (Util.transparentBlocks.contains(teleBlock.getType()) && (Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.UP).getType()) || Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.DOWN).getType()))) {
+        Block prev = null;
+        Block b;
+        while (iter.hasNext()) {
+            b = iter.next();
+            // Messaging.send(player, "Looping through blocks. Current Block: " + b.getType().toString());      // DEBUG
 
-			// Get the location
-			Location teleLoc = teleBlock.getLocation();
-			teleLoc.setYaw(targetLoc.getYaw());
-			teleLoc.setPitch(targetLoc.getPitch());
+            // Validate blocks near destination
+            if (Util.transparentBlocks.contains(b.getType()) && (Util.transparentBlocks.contains(b.getRelative(BlockFace.UP).getType()) || Util.transparentBlocks.contains(b.getRelative(BlockFace.DOWN).getType()))) {
+                prev = b;
+            }
+            else {
+                break;
+            }
+        }
+        if (prev != null) {
+            Location teleport = prev.getLocation().clone();
+            teleport.add(new Vector(.5, 0, .5));
 
-			// Teleport the player
-			Player player = hero.getPlayer();
-			if (player.teleport(teleLoc)) {
+            // Set the blink location yaw/pitch to that of the target
+            teleport.setPitch(targetLoc.getPitch());
+            teleport.setYaw(targetLoc.getYaw());
+            player.teleport(teleport);
 
-				// Play Sound
-				player.getWorld().playSound(hero.getPlayer().getLocation(), Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
+            broadcastExecuteText(hero, target);
 
-				// Play Effect
-				player.getWorld().playEffect(player.getLocation(), Effect.ENDER_SIGNAL, 3);
+            player.getWorld().playEffect(playerLoc, Effect.ENDER_SIGNAL, 3);
+            player.getWorld().playSound(playerLoc, Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
 
-				// Announce skill usage
-				broadcast(targetLoc, useText, hero.getName(), ((Player) target).getName());
+            return SkillResult.NORMAL;
+        }
+        else {
+            Messaging.send(player, "No location to shadowstep to.");
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
 
-				return SkillResult.NORMAL;
-			}
-			else {
-				Messaging.send(hero.getPlayer(), teleFailText);
-				return SkillResult.FAIL;
-			}
-		}
-		else {
-			Messaging.send(hero.getPlayer(), teleFailText);
-			return SkillResult.FAIL;
-		}
-	}
+        // OLD METHOD. Leaving here for now just in case I decide to switch things back.
+
+        //		Location targetLoc = target.getLocation();
+        //		BlockFace targetFace = faces[Math.round(targetLoc.getYaw() / 45f) & 0x7];
+        //		Block teleBlock = targetLoc.getBlock().getRelative(targetFace);
+        //
+        //		// Ensure that the location isn't inside of a block
+        //		if (Util.transparentBlocks.contains(teleBlock.getType()) && (Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.UP).getType()) || Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.DOWN).getType()))) {
+        //
+        //			// Get the location
+        //			Location teleLoc = teleBlock.getLocation();
+        //			teleLoc.setYaw(targetLoc.getYaw());
+        //			teleLoc.setPitch(targetLoc.getPitch());
+        //          teleLoc.add(new Vector(.5, 0, .5));
+        //
+        //			// Teleport the player
+        //			if (player.teleport(teleLoc)) {
+        //
+        //				// Play Sound
+        //                player.getWorld().playSound(player.getLocation(), Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
+        //
+        //				// Play Effect
+        //				player.getWorld().playEffect(player.getLocation(), Effect.ENDER_SIGNAL, 3);
+        //
+        //				// Announce skill usage
+        //				broadcast(targetLoc, useText, hero.getName(), ((Player) target).getName());
+        //
+        //				return SkillResult.NORMAL;
+        //			}
+        //			else {
+        //                Messaging.send(player, teleFailText);
+        //				return SkillResult.FAIL;
+        //			}
+        //		}
+        //		else {
+        //            Messaging.send(player, teleFailText);
+        //			return SkillResult.FAIL;
+        //		}
+    }
 }
