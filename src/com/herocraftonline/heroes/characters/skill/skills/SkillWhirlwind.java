9b3cde64b709b1bf89c4a7f41ae9bb2cb9bbf637
Delfofthebla <Delfofthebla@gmail.com>
2013-09-08 04:59:14 -0700

    Forgot to actually add the new skills again...

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillWhirlwind.java
@@ -0,0 +1,81 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import java.util.List;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.attributes.AttributeType;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+
+public class SkillWhirlwind extends ActiveSkill {
+
+    public SkillWhirlwind(Heroes plugin) {
+        super(plugin, "Whirlwind");
+        setDescription("Unleash a furious Whirlwind attack to all enemies within $1 blocks. Enemies struck are dealt $2 damage.");
+        setUsage("/skill whirlwind");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill whirlwind");
+        setTypes(SkillType.DAMAGING, SkillType.ABILITY_PROPERTY_PHYSICAL, SkillType.AGGRESSIVE);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 60, false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, 1.0, false);
+        damage += (int) (damageIncrease * hero.getAttributeValue(AttributeType.STRENGTH));
+
+        int radius = SkillConfigManager.getUseSetting(hero, this, SkillSetting.RADIUS, 5, false);
+
+        return getDescription().replace("$2", damage + "").replace("$1", radius + "");
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set(SkillSetting.DAMAGE.node(), 60);
+        node.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), Double.valueOf(1.0));
+        node.set(SkillSetting.RADIUS.node(), 5);
+
+        return node;
+    }
+
+    @Override
+    public SkillResult use(Hero hero, String[] args) {
+        Player player = hero.getPlayer();
+
+        broadcastExecuteText(hero);
+
+        double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, Integer.valueOf(60), false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, Double.valueOf(1.0), false);
+        damage += (damageIncrease * hero.getAttributeValue(AttributeType.STRENGTH));
+
+        int radius = SkillConfigManager.getUseSetting(hero, this, SkillSetting.RADIUS, 5, false);
+
+        List<Entity> entities = hero.getPlayer().getNearbyEntities(radius, radius, radius);
+        for (Entity entity : entities) {
+            if (!(entity instanceof LivingEntity)) {
+                continue;
+            }
+
+            LivingEntity target = (LivingEntity) entity;
+            if (!damageCheck(player, target))
+                continue;
+
+            addSpellTarget(target, hero);
+            damageEntity(target, player, damage, DamageCause.ENTITY_ATTACK);
+        }
+
+        return SkillResult.NORMAL;
+    }
+}
