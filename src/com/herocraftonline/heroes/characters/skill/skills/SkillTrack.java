54c43d4ade8290ba69f536aeaa887f3e201ce8e7
Delfofthebla <Delfofthebla@gmail.com>
2013-09-18 08:39:26 -0700

    Improve Track randomization and compass handling.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillTrack.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillTrack.java
@@ -1,5 +1,7 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
+import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
@@ -8,8 +10,13 @@ import org.bukkit.entity.Player;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.Monster;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.effects.PeriodicExpirableEffect;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.Skill;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.util.Messaging;
 import com.herocraftonline.heroes.util.Util;
 
@@ -17,7 +24,7 @@ public class SkillTrack extends ActiveSkill {
 
     public SkillTrack(Heroes plugin) {
         super(plugin, "Track");
-        setDescription("You are able to track down another player's location.");
+        setDescription("Track your target, and learn of their location in the world, specific up to $1 blocks. Your compass will point to the target for $2 seconds after tracking them.");
         setUsage("/skill track <player>");
         setArgumentRange(1, 1);
         setIdentifiers("skill track");
@@ -25,7 +32,12 @@ public class SkillTrack extends ActiveSkill {
 
     @Override
     public String getDescription(Hero hero) {
-        return getDescription();
+        int randomness = SkillConfigManager.getUseSetting(hero, this, "randomness", 50, true);
+        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.COOLDOWN, Integer.valueOf(30000), false);
+
+        String formattedDuration = Util.decFormat.format(duration / 1000.0);
+
+        return getDescription().replace("$1", randomness + "").replace("$2", formattedDuration);
     }
 
     @Override
@@ -49,16 +61,62 @@ public class SkillTrack extends ActiveSkill {
         	return SkillResult.NORMAL;
         }
 
-        Location location = target.getLocation();
-        int randomness = SkillConfigManager.getUseSetting(hero, this, "randomness", 50, true);
-        int x = location.getBlockX() + Util.nextInt(randomness);
-        int y = location.getBlockY() + Util.nextInt(randomness / 10);
-        int z = location.getBlockZ() + Util.nextInt(randomness);
-
-        Messaging.send(player, "Tracked $1: $2,$3,$4", target.getName(), x, y, z);
-        player.setCompassTarget(location);
-        hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.LEVEL_UP , 0.8F, 5.0F); ;
         broadcastExecuteText(hero);
+
+        int period = SkillConfigManager.getUseSetting(hero, this, SkillSetting.PERIOD, Integer.valueOf(1000), false);
+        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.COOLDOWN, Integer.valueOf(30000), false);
+
+        hero.addEffect(new TrackingEffect(this, player, period, duration, target));
+
+        player.getWorld().playSound(player.getLocation(), Sound.LEVEL_UP, 0.8F, 5.0F);
+
         return SkillResult.NORMAL;
+    }
+
+    public class TrackingEffect extends PeriodicExpirableEffect {
+        private Player target;
+
+        public TrackingEffect(Skill skill, Player applier, long period, long duration, Player target) {
+            super(skill, "Tracking", applier, period, duration, null, null);
+
+            types.add(EffectType.PHYSICAL);
+
+            this.target = target;
+        }
+
+        @Override
+        public void applyToHero(Hero hero) {
+            super.applyToHero(hero);
+            Player player = hero.getPlayer();
+
+            Location location = target.getLocation();
+
+            player.setCompassTarget(location);
+
+            long time = System.currentTimeMillis();
+            Random ranGen = new Random((int) ((time / 2.0) * 12));
+
+            int randomness = SkillConfigManager.getUseSetting(hero, skill, "randomness", 50, true);
+
+            int randomX = (int) (randomness * ranGen.nextGaussian());
+            int randomY = (int) ((randomness * Math.abs(ranGen.nextGaussian())) / 10);
+            int randomZ = (int) (randomness * ranGen.nextGaussian());
+
+            int x = location.getBlockX() + randomX;
+            int y = location.getBlockY() + randomY;
+            int z = location.getBlockZ() + randomZ;
+
+            Messaging.send(player, "Tracked $1: $2, $3, $4", target.getName(), x, y, z);
+        }
+
+        @Override
+        public void tickHero(Hero hero) {
+            Player player = hero.getPlayer();
+
+            player.setCompassTarget(target.getLocation());
+        }
+
+        @Override
+        public void tickMonster(Monster monster) {}
     }
 }
