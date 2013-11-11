4104405c272de50676c13413cf6d0008a23c5f4b
Kainzo <kainzo@gmail.com>
2013-11-11 01:01:05 -0600

    Lets test and add Forge

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillForge.java
@@ -0,0 +1,20 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.skill.PassiveSkill;
+
+public class SkillForge extends PassiveSkill {
+	public SkillForge(Heroes plugin) {
+		super(plugin, "Forge");
+		setDescription("Passive: Grants ability to use the anvil to improve weapons/tools/armor's sharpness/efficiency/protection respectively.");
+		setEffectTypes(new EffectType[] { EffectType.BENEFICIAL });
+	}
+
+	@Override 
+	public String getDescription(Hero hero) {
+		return getDescription();
+	}
+	
+}
