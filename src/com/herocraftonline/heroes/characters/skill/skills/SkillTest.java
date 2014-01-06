d6f94ebf05748e1a7e14ee271531828ec4823c76
Kainzo <Kainzo@gmail.com>
2014-01-05 21:35:39 -0600

    This should fix her up.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillTest.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillTest.java
@@ -18,7 +18,7 @@ import com.herocraftonline.heroes.util.Util;
 
 public class SkillTest extends ActiveSkill {
 
-    public SkillTest(Heroes plugin) {
+    public SkillEnderCrystal(Heroes plugin) {
         super(plugin, "Test");
         setDescription("I am become death");
         setUsage("/skill test");
@@ -28,15 +28,28 @@ public class SkillTest extends ActiveSkill {
     }
 
     @Override
+    public String getDescription(Hero hero) {
+        int chance2x = (int) (SkillConfigManager.getUseSetting(hero, this, "chance-2x", 0.2, false) * 100 + SkillConfigManager.getUseSetting(hero, this, "chance-2x-per-level", 0.0, false) * hero.getLevel());
+        int chance3x = (int) (SkillConfigManager.getUseSetting(hero, this, "chance-3x", 0.1, false) * 100 + SkillConfigManager.getUseSetting(hero, this, "chance-3x-per-level", 0.0, false) * hero.getLevel());
+        return getDescription().replace("$2", chance2x + "").replace("$3", chance3x + "");
+    }
+
+    @Override
     public ConfigurationSection getDefaultConfig() {
         ConfigurationSection node = super.getDefaultConfig();
+        node.set("chance-2x", 0.2);
+        node.set("chance-3x", 0.1);
         node.set(SkillSetting.MAX_DISTANCE.node(), 20);
+        node.set("chance-2x-per-level", 0.0);
+        node.set("chance-3x-per-level", 0.0);
         return node;
     }
 
     @Override
     public SkillResult use(Hero hero, String[] args) {
         Player player = hero.getPlayer();
+        double chance2x = SkillConfigManager.getUseSetting(hero, this, "chance-2x", 0.2, false) + (int) SkillConfigManager.getUseSetting(hero, this, "chance-2x-per-level", 0.0, false) * hero.getSkillLevel(this);
+        double chance3x = SkillConfigManager.getUseSetting(hero, this, "chance-3x", 0.1, false) + (int) SkillConfigManager.getUseSetting(hero, this, "chance-3x-per-level", 0.0, false) * hero.getSkillLevel(this);
         int distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 20, false);
         Block wTargetBlock = player.getTargetBlock(null, distance).getRelative(BlockFace.UP);
         player.getWorld().spawnEntity(wTargetBlock.getLocation(), EntityType.FALLING_BLOCK);
