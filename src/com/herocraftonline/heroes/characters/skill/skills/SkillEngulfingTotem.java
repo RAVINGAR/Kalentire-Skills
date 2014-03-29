58c41212c29e0c489d9a9e4495fde00d52aaa063
Kainzo <Kainzo@gmail.com>
2014-03-29 14:42:05 -0500

    Lets revert that for now.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillEngulfingTotem.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillEngulfingTotem.java
@@ -170,7 +170,7 @@ public class SkillEngulfingTotem extends SkillBaseTotem {
                         time = 0.0;
                     }
 
-                    //entity.getLocation(location).add(0.7 * Math.sin(time * 16), 0, 0.7 * Math.cos(time * 16)); #temp removal for lag until we can figure out why its lingering.
+                    entity.getLocation(location).add(0.7 * Math.sin(time * 16), 0, 0.7 * Math.cos(time * 16));
                     /* This is the new Particle API system for Spigot - the first few int = id, data, offsetX/Y/Z, speed, count, radius)
                      * offset controls how spread out the particles are
                      * id and data only work for two particles: ITEM_BREAK and TILE_BREAK
