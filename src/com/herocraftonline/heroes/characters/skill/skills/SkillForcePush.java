058edaa83db2569263f1b824ce5ecef2068e255c
Kainzo <Kainzo@gmail.com>
2014-07-13 22:49:09 -0500

    More changes for NCP

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillForcePush.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillForcePush.java
@@ -150,6 +150,7 @@ public class SkillForcePush extends TargettedSkill {
         player.getWorld().spigot().playEffect(target.getLocation().add(0, 0.9, 0), org.bukkit.Effect.WITCH_MAGIC, 0, 0, 0, 0, 0, 1, 25, 16);
         player.getWorld().spigot().playEffect(target.getLocation().add(0, 1.0, 0), org.bukkit.Effect.WITCH_MAGIC, 0, 0, 0, 0, 0, 1, 25, 16);
         player.getWorld().spigot().playEffect(target.getLocation().add(0, 1.1, 0), org.bukkit.Effect.WITCH_MAGIC, 0, 0, 0, 0, 0, 1, 25, 16);
+
         return SkillResult.NORMAL;
     }
 
