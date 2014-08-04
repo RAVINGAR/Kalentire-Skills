3d32cfa18fcab18e3b1ddca0f912c4782c47a3b3
mung3r <rodney@bighatchet.com>
2014-08-04 00:16:47 -0700

    refactored and remove dupe code for forge skills

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeChainHelmet.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeChainHelmet.java
@@ -1,27 +1,12 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
-import com.herocraftonline.heroes.Heroes;
-import com.herocraftonline.heroes.api.SkillResult;
-import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.skill.ActiveSkill;
-import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
-import com.herocraftonline.heroes.characters.skill.SkillSetting;
-import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Messaging;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.characters.skill.SkillType;
 
-public class SkillForgeChainHelmet extends ActiveSkill {
+public class SkillForgeChainHelmet extends SkillBaseForgeItem {
+
     public SkillForgeChainHelmet(Heroes plugin) {
         super(plugin, "ForgeChainHelmet");
         setDescription("You forge a chain helmet!");
@@ -29,49 +14,7 @@ public class SkillForgeChainHelmet extends ActiveSkill {
         setArgumentRange(0, 0);
         setIdentifiers("skill forgechainhelmet", "skill chainhelm");
         setTypes(SkillType.ITEM_CREATION);
+        defaultAmount = 2;
+        deafultItem = Material.CHAINMAIL_HELMET;
     }
-
-    public String getDescription(Hero hero) {
-        int amount = SkillConfigManager.getUseSetting(hero, this, SkillSetting.AMOUNT, 2, false);
-
-        return getDescription().replace("$1", amount + "");
-    }
-
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection node = super.getDefaultConfig();
-
-        node.set(SkillSetting.AMOUNT.node(), 1);
-
-        return node;
-    }
-
-    public SkillResult use(Hero hero, String[] args) {
-        Player player = hero.getPlayer();
-
-        ItemStack forgedItem = new ItemStack(Material.CHAINMAIL_HELMET, SkillConfigManager.getUseSetting(hero, this, "amount", 1, false));
-        ItemMeta metaData = forgedItem.getItemMeta();
-
-        // Add the "Forged by" message to the item.
-        String imbuedByInformation = ChatColor.DARK_PURPLE + "Forged by " + player.getName();
-        List<String> newLore = Arrays.asList(imbuedByInformation);
-        metaData.setLore(newLore);
-
-        // Set the new metaData to the item
-        forgedItem.setItemMeta(metaData);
-
-        // Add the item to their inventory, but only if they have space.
-        PlayerInventory inventory = player.getInventory();
-        HashMap<Integer, ItemStack> leftOvers = inventory.addItem(forgedItem);
-        for (java.util.Map.Entry<Integer, ItemStack> entry : leftOvers.entrySet()) {
-            player.getWorld().dropItemNaturally(player.getLocation(), entry.getValue());
-            Messaging.send(player, "Items have been dropped at your feet!");
-        }
-
-        broadcastExecuteText(hero);
-
-        hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.ANVIL_USE, 0.6F, 1.0F);
-
-        return SkillResult.NORMAL;
-    }
-
 }
--- src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeChainLeggings.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeChainLeggings.java
@@ -1,27 +1,12 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
-import com.herocraftonline.heroes.Heroes;
-import com.herocraftonline.heroes.api.SkillResult;
-import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.skill.ActiveSkill;
-import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
-import com.herocraftonline.heroes.characters.skill.SkillSetting;
-import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Messaging;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.Sound;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.characters.skill.SkillType;
 
-public class SkillForgeChainLeggings extends ActiveSkill {
+public class SkillForgeChainLeggings extends SkillBaseForgeItem {
+
     public SkillForgeChainLeggings(Heroes plugin) {
         super(plugin, "ForgeChainLeggings");
         setDescription("You forge a chain leggings!");
@@ -29,48 +14,7 @@ public class SkillForgeChainLeggings extends ActiveSkill {
         setArgumentRange(0, 0);
         setIdentifiers("skill forgechainleggings");
         setTypes(SkillType.ITEM_CREATION);
-    }
-
-    public String getDescription(Hero hero) {
-        int amount = SkillConfigManager.getUseSetting(hero, this, SkillSetting.AMOUNT, 1, false);
-
-        return getDescription().replace("$1", amount + "");
-    }
-
-    public ConfigurationSection getDefaultConfig() {
-        ConfigurationSection node = super.getDefaultConfig();
-
-        node.set(SkillSetting.AMOUNT.node(), 1);
-
-        return node;
-    }
-
-    public SkillResult use(Hero hero, String[] args) {
-        Player player = hero.getPlayer();
-
-        ItemStack forgedItem = new ItemStack(Material.CHAINMAIL_LEGGINGS, SkillConfigManager.getUseSetting(hero, this, "amount", 1, false));
-        ItemMeta metaData = forgedItem.getItemMeta();
-
-        // Add the "Forged by" message to the item.
-        String imbuedByInformation = ChatColor.DARK_PURPLE + "Forged by " + player.getName();
-        List<String> newLore = Arrays.asList(imbuedByInformation);
-        metaData.setLore(newLore);
-
-        // Set the new metaData to the item
-        forgedItem.setItemMeta(metaData);
-
-        // Add the item to their inventory, but only if they have space.
-        PlayerInventory inventory = player.getInventory();
-        HashMap<Integer, ItemStack> leftOvers = inventory.addItem(forgedItem);
-        for (java.util.Map.Entry<Integer, ItemStack> entry : leftOvers.entrySet()) {
-            player.getWorld().dropItemNaturally(player.getLocation(), entry.getValue());
-            Messaging.send(player, "Items have been dropped at your feet!");
-        }
-
-        broadcastExecuteText(hero);
-
-        hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.ANVIL_USE, 0.6F, 1.0F);
-
-        return SkillResult.NORMAL;
+        defaultAmount = 1;
+        deafultItem = Material.CHAINMAIL_LEGGINGS;
     }
 }
