08ed38b5432cd81ea8b311638be61c5008d65d0b
mung3r <mung3r@bighatchet.com>
2014-08-08 11:14:49 -0700

    cleaned up skeleton skill

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillSkeleton.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillSkeleton.java
@@ -1,33 +1,43 @@
-/*package com.herocraftonline.heroes.characters.skill.skills;
-
-import com.herocraftonline.heroes.Heroes;
-import com.herocraftonline.heroes.api.SkillResult;
-import com.herocraftonline.heroes.characters.skill.ActiveSkill;
-import com.herocraftonline.heroes.characters.effects.Effect;
-import com.herocraftonline.heroes.characters.effects.common.SummonEffect;
-import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
-import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.util.Messaging;
-import com.herocraftonline.heroes.characters.skill.SkillSetting;
-import org.bukkit.Location;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.*;
-import org.bukkit.event.Event;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
-import org.bukkit.event.entity.*;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerQuitEvent;
+package com.herocraftonline.heroes.characters.skill.skills;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.CharacterTemplate;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.Monster;
+import com.herocraftonline.heroes.characters.effects.Effect;
+import com.herocraftonline.heroes.characters.effects.common.SummonEffect;
+import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.util.Messaging;
+
 public class SkillSkeleton extends ActiveSkill {
     private String expireText;
-    private SummonEntityListener eListener;
-    private SummonPlayerListener pListener;
 
     public SkillSkeleton(Heroes paramHeroes)
     {
@@ -37,13 +47,11 @@ public class SkillSkeleton extends ActiveSkill {
         setArgumentRange(0, 0);
         setIdentifiers(new String[] { "skill skeleton" });
         setTypes(SkillType.ABILITY_PROPERTY_DARK, SkillType.SUMMONING, SkillType.SILENCEABLE);
-        this.eListener = new SummonEntityListener();
-        this.pListener = new SummonPlayerListener();
-        registerEvent(Event.Type.ENTITY_DEATH, this.eListener, Event.Priority.Monitor);
-        registerEvent(Event.Type.ENTITY_TARGET, this.eListener, Event.Priority.Highest);
-        registerEvent(Event.Type.ENTITY_COMBUST, this.eListener, Event.Priority.Highest);
-        registerEvent(Event.Type.ENTITY_DAMAGE, this.eListener, Event.Priority.Monitor);
-        registerEvent(Event.Type.PLAYER_QUIT, this.pListener, Event.Priority.Lowest);
+    }
+
+    @Override
+    public String getDescription(Hero arg0) {
+        return super.getDescription();
     }
 
     @Override
@@ -63,7 +71,7 @@ public class SkillSkeleton extends ActiveSkill {
     public void init()
     {
         super.init();
-        this.expireText = SkillConfigManager.getRaw(this, Setting.EXPIRE_TEXT, "The skeleton returns to it's hellish domain.");
+        this.expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, "The skeleton returns to it's hellish domain.");
     }
 
     public SkillResult use(Hero paramHero, String[] paramArrayOfString)
@@ -71,11 +79,11 @@ public class SkillSkeleton extends ActiveSkill {
         Player localPlayer = paramHero.getPlayer();
         if (paramHero.getSummons().size() < SkillConfigManager.getUseSetting(paramHero, this, "max-summons", 3, false))
         {
-            int i = SkillConfigManager.getUseSetting(paramHero, this, Setting.MAX_DISTANCE, 5, false);
+            int i = SkillConfigManager.getUseSetting(paramHero, this, SkillSetting.MAX_DISTANCE, 5, false);
             Location localLocation = localPlayer.getTargetBlock((HashSet)null, i).getLocation();
-            Creature localCreature = (Creature)localPlayer.getWorld().spawnCreature(localLocation, CreatureType.SKELETON);
-            long l = SkillConfigManager.getUseSetting(paramHero, this, Setting.DURATION, 60000, false);
-            this.plugin.getEffectManager().addEntityEffect(localCreature, new SummonEffect(this, l, paramHero, this.expireText));
+            CharacterTemplate localCreature = plugin.getCharacterManager().getCharacter((LivingEntity) localPlayer.getWorld().spawnEntity(localLocation, EntityType.SKELETON));
+            long l = SkillConfigManager.getUseSetting(paramHero, this, SkillSetting.DURATION, 60000, false);
+            localCreature.addEffect(new SummonEffect(this, l, paramHero, this.expireText));
             broadcastExecuteText(paramHero);
             Messaging.send(localPlayer, "You have succesfully summoned a skeleton to fight for you.", new Object[0]);
             return SkillResult.NORMAL;
@@ -84,31 +92,34 @@ public class SkillSkeleton extends ActiveSkill {
         return SkillResult.FAIL;
     }
 
-    public class SummonPlayerListener
-            extends PlayerListener
+    protected class SummonPlayerListener
+               implements Listener
     {
-        public SummonPlayerListener() {}
+        public SummonPlayerListener() {
+            Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+        }
 
+        @EventHandler(priority=EventPriority.LOWEST)
         public void onPlayerQuit(PlayerQuitEvent paramPlayerQuitEvent)
         {
             Heroes.debug.startTask("HeroesSkillListener.Skeleton");
-            Hero localHero = SkillSkeleton.this.plugin.getHeroManager().getHero(paramPlayerQuitEvent.getPlayer());
+            Hero localHero = plugin.getCharacterManager().getHero(paramPlayerQuitEvent.getPlayer());
             if (localHero.getSummons().isEmpty())
             {
                 Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
                 return;
             }
-            Iterator localIterator = localHero.getSummons().iterator();
+            Iterator<Monster> localIterator = localHero.getSummons().iterator();
             while (localIterator.hasNext())
             {
-                LivingEntity localLivingEntity = (LivingEntity)localIterator.next();
-                if ((localLivingEntity instanceof Skeleton))
+                Monster localLivingEntity = localIterator.next();
+                if (localLivingEntity.getEntity() instanceof Skeleton)
                 {
-                    Effect localEffect = SkillSkeleton.this.plugin.getEffectManager().getEntityEffect(localLivingEntity, "Summon");
+                    Effect localEffect = localLivingEntity.getEffect("Summon");
                     if (localEffect != null) {
-                        SkillSkeleton.this.plugin.getEffectManager().removeEntityEffect(localLivingEntity, localEffect);
+                        localLivingEntity.removeEffect(localEffect);
                     } else {
-                        localLivingEntity.remove();
+                        localLivingEntity.getEntity().remove();
                     }
                 }
             }
@@ -116,11 +127,14 @@ public class SkillSkeleton extends ActiveSkill {
         }
     }
 
-    public class SummonEntityListener
-            extends EntityListener
+    protected class SummonEntityListener
+               implements Listener
     {
-        public SummonEntityListener() {}
+        public SummonEntityListener() {
+            Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+        }
 
+        @EventHandler(priority=EventPriority.HIGHEST)
         public void onEntityCombust(EntityCombustEvent paramEntityCombustEvent)
         {
             Heroes.debug.startTask("HeroesSkillListener.Skeleton");
@@ -129,13 +143,14 @@ public class SkillSkeleton extends ActiveSkill {
                 Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
                 return;
             }
-            Creature localCreature = (Creature)paramEntityCombustEvent.getEntity();
-            if (SkillSkeleton.this.plugin.getEffectManager().entityHasEffect(localCreature, "Summon")) {
+            CharacterTemplate localCreature = plugin.getCharacterManager().getCharacter((LivingEntity) paramEntityCombustEvent.getEntity());
+            if (localCreature.hasEffect("Summon")) {
                 paramEntityCombustEvent.setCancelled(true);
             }
             Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
         }
 
+        @EventHandler(priority=EventPriority.MONITOR)
         public void onEntityDamage(EntityDamageEvent paramEntityDamageEvent)
         {
             Heroes.debug.startTask("HeroesSkillListener.Skeleton");
@@ -147,11 +162,11 @@ public class SkillSkeleton extends ActiveSkill {
             Object localObject1;
             Object localObject2;
             Object localObject3;
-            Iterator localIterator;
+            Iterator<Monster> localIterator;
             LivingEntity localLivingEntity;
             if ((paramEntityDamageEvent.getEntity() instanceof Player))
             {
-                localObject1 = SkillSkeleton.this.plugin.getHeroManager().getHero((Player)paramEntityDamageEvent.getEntity());
+                localObject1 = SkillSkeleton.this.plugin.getCharacterManager().getHero((Player)paramEntityDamageEvent.getEntity());
                 if (((Hero)localObject1).getSummons().isEmpty())
                 {
                     Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
@@ -192,7 +207,7 @@ public class SkillSkeleton extends ActiveSkill {
                     Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
                     return;
                 }
-                localObject3 = SkillSkeleton.this.plugin.getHeroManager().getHero((Player)localObject2);
+                localObject3 = SkillSkeleton.this.plugin.getCharacterManager().getHero((Player)localObject2);
                 if (((Hero)localObject3).getSummons().isEmpty())
                 {
                     Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
@@ -210,6 +225,7 @@ public class SkillSkeleton extends ActiveSkill {
             }
         }
 
+        @EventHandler(priority=EventPriority.MONITOR)
         public void onEntityDeath(EntityDeathEvent paramEntityDeathEvent)
         {
             Heroes.debug.startTask("HeroesSkillListener.Skeleton");
@@ -219,11 +235,11 @@ public class SkillSkeleton extends ActiveSkill {
                 return;
             }
             Skeleton localSkeleton = (Skeleton)paramEntityDeathEvent.getEntity();
-            Collection localCollection = SkillSkeleton.this.plugin.getHeroManager().getHeroes();
-            Iterator localIterator = localCollection.iterator();
+            Collection<Hero> localCollection = SkillSkeleton.this.plugin.getCharacterManager().getHeroes();
+            Iterator<Hero> localIterator = localCollection.iterator();
             while (localIterator.hasNext())
             {
-                Hero localHero = (Hero)localIterator.next();
+                Hero localHero = localIterator.next();
                 if (localHero.getSummons().contains(localSkeleton)) {
                     localHero.getSummons().remove(localSkeleton);
                 }
@@ -231,6 +247,7 @@ public class SkillSkeleton extends ActiveSkill {
             Heroes.debug.stopTask("HeroesSkillListener.Skeleton");
         }
 
+        @EventHandler(priority=EventPriority.HIGHEST)
         public void onEntityTarget(EntityTargetEvent paramEntityTargetEvent)
         {
             Heroes.debug.startTask("HeroesSkillListener.Skeleton");
@@ -241,14 +258,14 @@ public class SkillSkeleton extends ActiveSkill {
             }
             if ((paramEntityTargetEvent.getTarget() instanceof Player))
             {
-                Iterator localIterator1 = SkillSkeleton.this.plugin.getHeroManager().getHeroes().iterator();
+                Iterator<Hero> localIterator1 = SkillSkeleton.this.plugin.getCharacterManager().getHeroes().iterator();
                 while (localIterator1.hasNext())
                 {
                     Hero localHero1 = (Hero)localIterator1.next();
                     if (localHero1.getSummons().contains(paramEntityTargetEvent.getEntity())) {
                         if (localHero1.getParty() != null)
                         {
-                            Iterator localIterator2 = localHero1.getParty().getMembers().iterator();
+                            Iterator<Hero> localIterator2 = localHero1.getParty().getMembers().iterator();
                             while (localIterator2.hasNext())
                             {
                                 Hero localHero2 = (Hero)localIterator2.next();
@@ -268,4 +285,3 @@ public class SkillSkeleton extends ActiveSkill {
         }
     }
 }
-*/
