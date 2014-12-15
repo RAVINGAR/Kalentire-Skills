fbf91c13514253c5761bcbf645433abd934c2a22
OxNaomi <dkabot@dkabot.com>
2014-12-15 17:00:12 -0500

    Spacing, Ashura description actually does say phys, added Murasame

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillMurasame.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillMurasame.java
@@ -1,5 +1,6 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
@@ -9,9 +10,9 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
 import com.herocraftonline.heroes.attributes.AttributeType;
 import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.effects.common.SlowEffect;
 import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.characters.skill.SkillType;
@@ -19,17 +20,14 @@ import com.herocraftonline.heroes.characters.skill.TargettedSkill;
 import com.herocraftonline.heroes.util.Messaging;
 import com.herocraftonline.heroes.util.Util;
 
-public class SkillKotesu extends TargettedSkill {
+public class SkillMurasame extends TargettedSkill {
 
-    private String applyText;
-    private String expireText;
-
-    public SkillKotesu(Heroes plugin) {
-        super(plugin, "Kotesu");
-        setDescription("Deal $1 dark damage and slow the target. Costs $2% of your sword.");
-        setUsage("/skill kotesu");
+    public SkillMurasame(Heroes plugin) {
+        super(plugin, "Murasame");
+        setDescription("Deal $1 physical damage and heal yourself for $2% of your total health. Costs $3% of your sword.");
+        setUsage("/skill murasame");
         setArgumentRange(0, 0);
-        setIdentifiers("skill kotesu");
+        setIdentifiers("skill murasame");
         setTypes(SkillType.ABILITY_PROPERTY_DARK, SkillType.ABILITY_PROPERTY_MAGICAL, SkillType.DAMAGING, SkillType.AGGRESSIVE, SkillType.MOVEMENT_SLOWING);
     }
 
@@ -39,9 +37,11 @@ public class SkillKotesu extends TargettedSkill {
         double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, 1.6, false);
         damage += (int) (damageIncrease * hero.getAttributeValue(AttributeType.STRENGTH));
 
-        int swordSacrificePercent = SkillConfigManager.getUseSetting(hero, this, "sword-sacrifice-percent", 5, false);
+        int healingPercent = SkillConfigManager.getUseSetting(hero, this, "healing-percent", 25, false);
 
-        return getDescription().replace("$1", damage + "").replace("$2", swordSacrificePercent + "");
+        int swordSacrificePercent = SkillConfigManager.getUseSetting(hero, this, "sword-sacrifice-percent", 10, false);
+
+        return getDescription().replace("$1", damage + "").replace("$2", healingPercent + "").replace("$3", swordSacrificePercent + "");
     }
 
     @Override
@@ -51,20 +51,10 @@ public class SkillKotesu extends TargettedSkill {
         node.set(SkillSetting.MAX_DISTANCE.node(), 5);
         node.set(SkillSetting.DAMAGE.node(), 50);
         node.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), 1.6);
-        node.set(SkillSetting.APPLY_TEXT.node(), Messaging.getSkillDenoter() + "$1 is slowed by Kotesu's power!");
-        node.set(SkillSetting.EXPIRE_TEXT.node(), Messaging.getSkillDenoter() + "Kotesu's power fades from $1.");
-        node.set("sword-sacrifice-percent", 5);
-        node.set("slowness-duration", 2000);
-        node.set("slowness-amplitude", 2);
+        node.set("sword-sacrifice-percent", 10);
+        node.set("healing-percent", 25);
+
         return node;
-    }
-
-    @Override
-    public void init() {
-        super.init();
-
-        applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, Messaging.getSkillDenoter() + "$1 is slowed by Kotesu's power!");
-        expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, Messaging.getSkillDenoter() + "Kotesu's power fades from $1.");
     }
 
     @Override
@@ -78,13 +68,13 @@ public class SkillKotesu extends TargettedSkill {
 
         Material item = player.getItemInHand().getType();
         if (!SkillConfigManager.getUseSetting(hero, this, "weapons", Util.swords).contains(item.name())) {
-            Messaging.send(player, "You can't use Kotesu with that weapon!");
+            Messaging.send(player, "You can't use Murasame with that weapon!");
             return SkillResult.INVALID_TARGET_NO_MSG;
         }
 
         short dura = player.getItemInHand().getDurability();
         short maxDura = player.getItemInHand().getType().getMaxDurability();
-        short duraCost = (short) (maxDura * (SkillConfigManager.getUseSetting(hero, this, "sword-sacrifice-percent", 5, false) * 0.01));
+        short duraCost = (short) (maxDura * (SkillConfigManager.getUseSetting(hero, this, "sword-sacrifice-percent", 10, false) * 0.01));
 
         if(dura == (short)0) {
             player.getItemInHand().setDurability((short) (duraCost));
@@ -97,7 +87,7 @@ public class SkillKotesu extends TargettedSkill {
             player.getWorld().playSound(player.getLocation(), Sound.ITEM_BREAK, 0.5F, 1.0F);
         }
         else {
-            Messaging.send(player, "Your Katana doesn't have enough durability to use Kotesu!");
+            Messaging.send(player, "Your Katana doesn't have enough durability to use Murasame!");
             return SkillResult.INVALID_TARGET_NO_MSG;
         }
         broadcastExecuteText(hero, target);
@@ -106,20 +96,25 @@ public class SkillKotesu extends TargettedSkill {
         double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, 1.6, false);
         damage += damageIncrease * hero.getAttributeValue(AttributeType.STRENGTH);
 
-        long duration = SkillConfigManager.getUseSetting(hero, this, "slowness-duration", 2000, false);
-        int amplifier = SkillConfigManager.getUseSetting(hero, this, "slowness-amplitude", 1, false);
+        int healingPercent = SkillConfigManager.getUseSetting(hero, this, "healing-percent", 25, false);
 
         if (damage > 0) {
             addSpellTarget(target, hero);
             damageEntity(target, player, damage, DamageCause.MAGIC, false);
         }
 
-        SlowEffect slowEffect = new SlowEffect(this, player, duration, amplifier, applyText, expireText);
+        if (healingPercent > 0) {
 
-        plugin.getCharacterManager().getCharacter(target).addEffect(slowEffect);
+            double healAmount = player.getMaxHealth() * (healingPercent / 100);
+            
+            HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(hero, healAmount, this, hero);
+            Bukkit.getPluginManager().callEvent(hrhEvent);
+            if(!hrhEvent.isCancelled()) {
+                hero.heal(hrhEvent.getAmount());
+            }
+        }
 
-        player.getWorld().spigot().playEffect(target.getLocation().add(0, 0.5, 0), org.bukkit.Effect.MAGIC_CRIT, 0, 0, 0, 0, 0, 1, 150, 16);
-
+        player.getWorld().spigot().playEffect(target.getLocation().add(0, 0.5, 0), org.bukkit.Effect.VILLAGER_THUNDERCLOUD, 0, 0, 0, 0, 0, 1, 150, 16);
 
         return SkillResult.NORMAL;
     }
