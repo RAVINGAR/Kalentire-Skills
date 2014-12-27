04aeea755f128d47c88c454920e6f318fc4bbabe
unknownloner <unknownloner.online@gmail.com>
2014-12-26 20:36:17 -0500

    Add optional position setting to Hellgate Set 'teleport-abolute' to true, and then set x, y, and z

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillHellgate.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillHellgate.java
@@ -39,9 +39,12 @@ public class SkillHellgate extends ActiveSkill {
         ConfigurationSection node = super.getDefaultConfig();
 
         node.set(SkillSetting.RADIUS.node(), 10);
+        node.set("teleport-absolute", false);
+        node.set("x", 0);
+        node.set("y", 128);
+        node.set("z", 0);
         node.set("hell-world", "hell");
-        node.set("default-return", "bastion"); // default world the player return to if their location wasn't
-                                                     // saved
+        node.set("default-return", "bastion"); // default world the player return to if their location wasn't saved
         return node;
     }
 
@@ -86,7 +89,20 @@ public class SkillHellgate extends ActiveSkill {
             }
 
             hero.addEffect(new HellgateEffect(this, player.getLocation()));
-            teleportLocation = world.getSpawnLocation();
+            
+            boolean absolute = SkillConfigManager.getUseSetting(hero, this, "teleport-absolute", false);
+            if (absolute) {
+            	Location cur = hero.getPlayer().getLocation();
+            	
+            	//If no config setting, just leave them where they are
+            	int x = SkillConfigManager.getUseSetting(hero, this, "x", cur.getBlockX(), false);
+            	int y = SkillConfigManager.getUseSetting(hero, this, "y", cur.getBlockY(), false);
+            	int z = SkillConfigManager.getUseSetting(hero, this, "z", cur.getBlockZ(), false);
+            	
+            	teleportLocation = new Location(world, x, y, z);
+            } else {
+            	teleportLocation = world.getSpawnLocation();
+            }
         }
 
         if (hero.hasParty()) {
