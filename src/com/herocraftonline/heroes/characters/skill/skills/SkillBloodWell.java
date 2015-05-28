f5d96b88e968633e59c9357a7f1334990081d75c
Kainzo <kainzo@gmail.com>
2015-05-28 13:46:50 -0500

    Lets add these skills back in.

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillBloodWell.java
@@ -0,0 +1,207 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
+import com.herocraftonline.heroes.api.events.SkillDamageEvent;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.effects.ExpirableEffect;
+import com.herocraftonline.heroes.characters.skill.*;
+import com.herocraftonline.heroes.util.Messaging;
+import com.herocraftonline.heroes.util.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+public class SkillBloodWell extends ActiveSkill {
+    private String applyText;
+    private String expireText;
+
+    public SkillBloodWell(Heroes plugin) {
+        super(plugin, "BloodWell");
+        setDescription("Drink the blood of your enemies and restore your health! The BloodWell effect lasts for $1 seconds" +
+                " and causes you to gain health for $2% of your physical damage dealt. You cannot gain more than $3 health from this effect.");
+        setUsage("/skill bloodwell");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill bloodwell");
+        setTypes(SkillType.BUFFING, SkillType.AGGRESSIVE, SkillType.HEALING, SkillType.ABILITY_PROPERTY_PHYSICAL);
+
+        Bukkit.getPluginManager().registerEvents(new SkillHeroListener(), plugin);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+
+        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 7500, false);
+        double damageHealingPercent = SkillConfigManager.getUseSetting(hero, this, "damage-healing-percent", 0.1, false);
+        int maximumHealing = SkillConfigManager.getUseSetting(hero, this, "maximum-healing", 200, false);
+
+        String formattedDuration = Util.decFormat.format(duration / 1000.0);
+        String formattedhealingPercent = Util.decFormat.format(damageHealingPercent * 100.0);
+
+        return getDescription().replace("$1", formattedDuration).replace("$2", formattedhealingPercent).replace("$3", maximumHealing + "");
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set("weapons", Util.axes);
+        node.set("damage-healing-percent", 0.1);
+        node.set("maximum-healing", 200);
+        node.set(SkillSetting.DURATION.node(), 7500);
+        node.set(SkillSetting.APPLY_TEXT.node(), Messaging.getSkillDenoter() + "%hero% is drinking blood!");
+        node.set(SkillSetting.EXPIRE_TEXT.node(), Messaging.getSkillDenoter() + "%hero% is no longer drinking blood!");
+
+        return node;
+    }
+
+    public void init() {
+        super.init();
+
+        applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, Messaging.getSkillDenoter() + "%hero% is drinking blood!").replace("%hero%", "$1");
+        expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, Messaging.getSkillDenoter() + "%hero% is no longer drinking blood!").replace("%hero%", "$1");
+    }
+
+    public SkillResult use(Hero hero, String[] args) {
+
+        Player player = hero.getPlayer();
+
+        broadcastExecuteText(hero);
+
+        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 7500, false);
+        double damageHealingPercent = SkillConfigManager.getUseSetting(hero, this, "damage-healing-percent", 0.1, false);
+        int maximumHealing = SkillConfigManager.getUseSetting(hero, this, "maximum-healing", 200, false);
+
+        BloodDrinkEffect effect = new BloodDrinkEffect(this, player, duration, damageHealingPercent, maximumHealing);
+
+        hero.addEffect(effect);
+
+        player.getWorld().playSound(player.getLocation(), Sound.BURP, 0.9F, 0.5F);
+        player.getWorld().playSound(player.getLocation(), Sound.SPLASH, 0.5F, 2.0F);
+
+        return SkillResult.NORMAL;
+    }
+
+    private class SkillHeroListener implements Listener {
+
+        public SkillHeroListener() {}
+
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onSkillDamage(SkillDamageEvent event) {
+            if (event.getDamage() == 0)
+                return;
+
+            // Handle outgoing
+            if (event.getDamager() instanceof Hero) {
+                Hero hero = (Hero) event.getDamager();
+                if (hero.hasEffect("BloodDrinking")) {
+                    if (!damageCheck(hero.getPlayer(), (LivingEntity) event.getEntity()))
+                        return;
+
+                    bloodDrinkHeal(hero, event.getDamage());
+                }
+            }
+        }
+
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onEntityDamage(EntityDamageEvent event) {
+            if (event.getDamage() == 0 || !(event.getEntity() instanceof LivingEntity) || !(event instanceof EntityDamageByEntityEvent) || event.getCause() != DamageCause.ENTITY_ATTACK) {
+                return;
+            }
+
+            EntityDamageByEntityEvent edbe = (EntityDamageByEntityEvent) event;
+            Entity attacker = edbe.getDamager();
+            
+            // Handle outgoing
+            if (attacker instanceof Player) {
+                Player player = (Player) attacker;
+                Hero hero = plugin.getCharacterManager().getHero(player);
+                if (hero.hasEffect("BloodDrinking"))
+                    bloodDrinkHeal(hero, event.getDamage());
+            }
+        }
+    }
+
+    private void bloodDrinkHeal(Hero hero, double damage) {
+        BloodDrinkEffect bdEffect = (BloodDrinkEffect) hero.getEffect("BloodDrinking");
+
+        if (bdEffect != null) {
+            int maxHealing = bdEffect.getMaximumHealing();
+            double currentTotalHeal = bdEffect.getTotalHealthHealed();
+
+            if (currentTotalHeal < maxHealing) {
+                double healing = damage * bdEffect.getDamageHealingPercent();
+                if (healing + currentTotalHeal > maxHealing) {
+                    healing = maxHealing - currentTotalHeal;
+                }
+
+                HeroRegainHealthEvent healEvent = new HeroRegainHealthEvent(hero, healing, this);       // Bypass self heal nerf because this cannot be used on others.
+                Bukkit.getPluginManager().callEvent(healEvent);
+                if (!healEvent.isCancelled()) {
+                    double finalHealing = healEvent.getAmount();
+                    hero.heal(finalHealing);
+
+                    Player player = hero.getPlayer();
+                    player.getWorld().playSound(player.getLocation(), Sound.DRINK, 0.3F, 0.6F);
+
+                    bdEffect.setTotalHealthHealed(currentTotalHeal + finalHealing);
+
+                    if (bdEffect.getTotalHealthHealed() >= maxHealing)
+                        hero.removeEffect(bdEffect);
+                }
+            }
+        }
+    }
+
+    public class BloodDrinkEffect extends ExpirableEffect {
+        private double damageHealingPercent;
+        private double totalHealthHealed;
+        private int maximumHealing;
+
+        public BloodDrinkEffect(Skill skill, Player applier, long duration, double damageHealingPercent, int maximumHealing) {
+            super(skill, "BloodDrinking", applier, duration, applyText, expireText);
+
+            types.add(EffectType.PHYSICAL);
+            types.add(EffectType.BENEFICIAL);
+            types.add(EffectType.HEALING);
+
+            this.damageHealingPercent = damageHealingPercent;
+            this.maximumHealing = maximumHealing;
+        }
+
+        public int getMaximumHealing() {
+            return maximumHealing;
+        }
+
+        public void setMaximumHealing(int maximumHealing) {
+            this.maximumHealing = maximumHealing;
+        }
+
+        public double getDamageHealingPercent() {
+            return damageHealingPercent;
+        }
+
+        public void setDamageHealingPercent(double damageHealingPercent) {
+            this.damageHealingPercent = damageHealingPercent;
+        }
+
+        public double getTotalHealthHealed() {
+            return totalHealthHealed;
+        }
+
+        public void setTotalHealthHealed(double totalHealthHealed) {
+            this.totalHealthHealed = totalHealthHealed;
+        }
+    }
+}
---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillShadowstep.java
@@ -0,0 +1,157 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.attributes.AttributeType;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+import com.herocraftonline.heroes.util.Messaging;
+import com.herocraftonline.heroes.util.Util;
+
+public class SkillShadowstep extends TargettedSkill {
+
+    //private final BlockFace[] faces = { BlockFace.NORTH, BlockFace.NORTH_EAST, BlockFace.EAST, BlockFace.SOUTH_EAST, BlockFace.SOUTH, BlockFace.SOUTH_WEST, BlockFace.WEST, BlockFace.NORTH_WEST };
+
+    public SkillShadowstep(Heroes plugin) {
+        super(plugin, "Shadowstep");
+        setDescription("Shadow step your target, teleporting behind them. Has a maximum targetting range of $1 blocks.");
+        setUsage("/skill shadowstep");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill shadowstep");
+        setTypes(SkillType.ABILITY_PROPERTY_DARK, SkillType.MULTI_GRESSIVE, SkillType.NO_SELF_TARGETTING, SkillType.TELEPORTING, SkillType.SILENCEABLE);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        int distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 4, false);
+        double distanceIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE_INCREASE_PER_INTELLECT, 0.15, false);
+        distance += (int) (hero.getAttributeValue(AttributeType.INTELLECT) * distanceIncrease);
+
+        return getDescription().replace("$1", distance + "");
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection defaultConfig = super.getDefaultConfig();
+
+        defaultConfig.set(SkillSetting.MAX_DISTANCE.node(), 4);
+        defaultConfig.set(SkillSetting.MAX_DISTANCE_INCREASE_PER_INTELLECT.node(), 0.15);
+        defaultConfig.set("teleport-blocks-behind-target", 1);
+        defaultConfig.set(SkillSetting.USE_TEXT.node(), Messaging.getSkillDenoter() + "%hero% ShadowStepped behind %target%!");
+
+        return defaultConfig;
+    }
+
+    @Override
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+
+        Player player = hero.getPlayer();
+
+        if (target == player || !(target instanceof Player)) {
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
+
+        Location playerLoc = player.getLocation();
+        Location targetLoc = target.getLocation().clone();
+        targetLoc.setPitch(0);      // Reset pitch so that we don't have to worry about it.
+
+        BlockIterator iter = null;
+        try {
+            Vector direction = targetLoc.getDirection().multiply(-1);
+            int blocksBehindTarget = SkillConfigManager.getUseSetting(hero, this, "teleport-blocks-behind-target", 1, false);
+            iter = new BlockIterator(target.getWorld(), targetLoc.toVector(), direction, 0, blocksBehindTarget);
+        }
+        catch (IllegalStateException e) {
+            Messaging.send(player, "There was an error getting the Shadowstep location!");
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
+
+        Block prev = null;
+        Block b;
+        while (iter.hasNext()) {
+            b = iter.next();
+            // Messaging.send(player, "Looping through blocks. Current Block: " + b.getType().toString());      // DEBUG
+
+            // Validate blocks near destination
+            if (Util.transparentBlocks.contains(b.getType()) && (Util.transparentBlocks.contains(b.getRelative(BlockFace.UP).getType()) || Util.transparentBlocks.contains(b.getRelative(BlockFace.DOWN).getType()))) {
+                prev = b;
+            }
+            else {
+                break;
+            }
+        }
+        if (prev != null) {
+            Location targetTeleportLoc = prev.getLocation().clone();
+            targetTeleportLoc.add(new Vector(.5, 0, .5));
+
+            // Set the blink location yaw/pitch to that of the target
+            targetTeleportLoc.setPitch(0);
+            targetTeleportLoc.setYaw(targetLoc.getYaw());
+            player.teleport(targetTeleportLoc);
+
+            broadcastExecuteText(hero, target);
+
+            player.getWorld().playEffect(playerLoc, Effect.ENDER_SIGNAL, 3);
+            player.getWorld().playSound(playerLoc, Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
+
+            return SkillResult.NORMAL;
+        }
+        else {
+            Messaging.send(player, "No location to shadowstep to.");
+            return SkillResult.INVALID_TARGET_NO_MSG;
+        }
+
+        // OLD METHOD. Leaving here for now just in case I decide to switch things back.
+
+        //		Location targetLoc = target.getLocation();
+        //		BlockFace targetFace = faces[Math.round(targetLoc.getYaw() / 45f) & 0x7];
+        //		Block teleBlock = targetLoc.getBlock().getRelative(targetFace);
+        //
+        //		// Ensure that the location isn't inside of a block
+        //		if (Util.transparentBlocks.contains(teleBlock.getType()) && (Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.UP).getType()) || Util.transparentBlocks.contains(teleBlock.getRelative(BlockFace.DOWN).getType()))) {
+        //
+        //			// Get the location
+        //			Location teleLoc = teleBlock.getLocation();
+        //			teleLoc.setYaw(targetLoc.getYaw());
+        //			teleLoc.setPitch(targetLoc.getPitch());
+        //          teleLoc.add(new Vector(.5, 0, .5));
+        //
+        //			// Teleport the player
+        //			if (player.teleport(teleLoc)) {
+        //
+        //				// Play Sound
+        //                player.getWorld().playSound(player.getLocation(), Sound.ENDERMAN_TELEPORT, 0.8F, 1.0F);
+        //
+        //				// Play Effect
+        //				player.getWorld().playEffect(player.getLocation(), Effect.ENDER_SIGNAL, 3);
+        //
+        //				// Announce skill usage
+        //				broadcast(targetLoc, useText, hero.getName(), ((Player) target).getName());
+        //
+        //				return SkillResult.NORMAL;
+        //			}
+        //			else {
+        //                Messaging.send(player, teleFailText);
+        //				return SkillResult.FAIL;
+        //			}
+        //		}
+        //		else {
+        //            Messaging.send(player, teleFailText);
+        //			return SkillResult.FAIL;
+        //		}
+    }
+}
