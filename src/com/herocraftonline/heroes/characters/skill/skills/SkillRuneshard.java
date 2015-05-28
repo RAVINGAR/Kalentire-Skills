989697aed951fa399af115834d1bbddfc0330da4
Kainzo <kainzo@gmail.com>
2015-05-28 18:45:22 -0500

    Fix runeshard entity_attack

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillRuneshard.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillRuneshard.java
@@ -112,7 +112,7 @@ public class SkillRuneshard extends ActiveSkill {
                 arrow = true;
             }
             else {
-                if (event.getCause() != DamageCause.MAGIC)
+                if (event.getCause() != DamageCause.ENTITY_ATTACK)
                     return;
 
                 LivingEntity target = (LivingEntity) event.getEntity();
@@ -149,7 +149,7 @@ public class SkillRuneshard extends ActiveSkill {
                         return;
 
                     double damage = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE, 5, false);
-                    double damageIncrease = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 0.5, false);
+                    double damageIncrease = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 2.0, false);
                     damage += damageIncrease * hero.getAttributeValue(AttributeType.INTELLECT);
 
                     // Damage the target
---
+++ src/com/herocraftonline/heroes/characters/skill/skills/Utils/EntityUtil.java
@@ -0,0 +1,86 @@
+/*
+package com.herocraftonline.heroes.characters.skill.skills.Utils;
+
+
+import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.EntityTypes;
+import org.bukkit.entity.EntityType;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class EntityUtil {
+
+    protected static Field nameToClassField, classToNameField, classToIdField, nameToIdField, idToClassField;
+    //protected static Field mapIdToClassField;
+
+    static
+    {
+        try
+        {
+            nameToClassField = EntityTypes.class.getDeclaredField("c");
+            classToNameField = EntityTypes.class.getDeclaredField("d");
+            idToClassField = EntityTypes.class.getDeclaredField("e");
+            classToIdField = EntityTypes.class.getDeclaredField("f");
+            nameToIdField = EntityTypes.class.getDeclaredField("g");
+
+            nameToClassField.setAccessible(true);
+            classToNameField.setAccessible(true);
+            idToClassField.setAccessible(true);
+            classToIdField.setAccessible(true);
+            nameToIdField.setAccessible(true);
+        }
+        catch(Exception e) {e.printStackTrace();}
+    }
+    /**
+     * Registers a custom entity with the minecraft server, allowing the server to process it and the client to render it
+     * @param entityClass
+     *      The entity's class
+     * @param name
+     *      Unique name used to represent the custom entity
+     * @param id
+     *      Entity ID (Used to determine Entity Type rendered by client)
+     * @param makeDefault
+     *      If true, this will make the custom entity the default type spawned
+     */
+/**
+    public static void registerCustomEntity(Class<? extends Entity> entityClass, String name, int id, boolean makeDefault) {
+        if (nameToClassField == null || nameToIdField == null || classToNameField == null || classToIdField == null)
+        {
+            return;
+        }
+        else
+        {
+            try
+            {
+                @SuppressWarnings("unchecked")
+                Map<String, Class<? extends Entity>> nameToClassMap = (Map<String, Class<? extends Entity>>) nameToClassField.get(null);
+                @SuppressWarnings("unchecked")
+                Map<String, Integer> mapStringToId = (Map<String, Integer>) nameToIdField.get(null);
+                @SuppressWarnings("unchecked")
+                Map<Class<? extends Entity>, String> mapClasstoString = (Map<Class<? extends Entity>, String>) classToNameField.get(null);
+                @SuppressWarnings("unchecked")
+                Map<Class<? extends Entity>, Integer> mapClassToId = (Map<Class<? extends Entity>, Integer>) classToIdField.get(null);
+
+                nameToClassMap.put(name, entityClass);
+                mapStringToId.put(name, Integer.valueOf(id));
+                mapClasstoString.put(entityClass, name);
+                mapClassToId.put(entityClass, Integer.valueOf(id));
+
+                if (makeDefault) {
+                    String defName = EntityType.fromId(id).getName();
+                    nameToClassMap.put(defName, entityClass);
+                    @SuppressWarnings("unchecked")
+                    Map<Integer, Class<? extends Entity>> mapIdToClass = (Map<Integer, Class<? extends Entity>>) idToClassField.get(null);
+                    mapIdToClass.put(Integer.valueOf(id), entityClass);
+                }
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+        **/
