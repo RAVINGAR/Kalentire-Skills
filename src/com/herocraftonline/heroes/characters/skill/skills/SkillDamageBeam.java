b2d8996109c32ceca70cc91ea188f7e1696650f3
Walter Geisler <soren.endon@gmail.com>
2015-08-16 11:15:21 -0500

    Added particles and sounds for beam testing.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillDamageBeam.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDamageBeam.java
@@ -5,9 +5,15 @@ import static org.bukkit.event.entity.EntityDamageEvent.DamageCause.MAGIC;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.util.MathUtils;
 import com.herocraftonline.heroes.util.Util;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class SkillDamageBeam extends SkillBaseBeam {
 
@@ -27,6 +33,17 @@ public class SkillDamageBeam extends SkillBaseBeam {
 	public SkillResult use(Hero hero, String[] strings) {
 		Player player = hero.getPlayer();
 		Beam beam = new Beam(player, Util.transparentBlocks, 20, 5);
+
+		broadcastExecuteText(hero);
+
+		List<Location> fxLine = MathUtils.getLinePoints(player.getEyeLocation(),
+				player.getEyeLocation().add(beam.getDirectionX(), beam.getDirectionY(), beam.getDirectionZ()), (int) beam.calculateLength() * 2);
+		for (int i = 0; i < fxLine.size(); i++) {
+			player.getWorld().spigot().playEffect(fxLine.get(i), Effect.CRIT, 0, 0, 0.4f, 0.4f, 0.4f, 1, 25, 16);
+		}
+
+		player.getWorld().playSound(player.getEyeLocation(), Sound.AMBIENCE_THUNDER, 6, 2);
+
 		castBeam(hero, beam);
 		return SkillResult.NORMAL;
 	}
