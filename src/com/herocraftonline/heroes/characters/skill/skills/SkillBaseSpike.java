367e46056ae88a50b84f434836f00ce7f106fb06
Walter Geisler <soren.endon@gmail.com>
2015-08-22 04:59:57 -0500

    Damage spike prototype

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillBaseSpike.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillBaseSpike.java
@@ -3,31 +3,76 @@ package com.herocraftonline.heroes.characters.skill.skills;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.characters.skill.TargettedSkill;
 import de.slikey.effectlib.EffectManager;
+import de.slikey.effectlib.EffectType;
 import de.slikey.effectlib.effect.ConeEffect;
 import de.slikey.effectlib.util.ParticleEffect;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 public abstract class SkillBaseSpike extends TargettedSkill {
+
+	private static final long TICK_RISE_TIME = 5;
 
 	public SkillBaseSpike(Heroes plugin, String name) {
 		super(plugin, name);
 	}
 
+	private static ConeEffect createCone(EffectManager em, ParticleEffect particle, int particlesCone, double height, double radius) {
+		checkArgument(particlesCone > 0, "particlesCone must be greater than 0");
 
-
-	protected void renderSpike(Location target, double height, double radius, ParticleEffect particle, Color color) {
-
-		EffectManager em = new EffectManager(plugin);
 		ConeEffect effect = new ConeEffect(em);
+
+		effect.particle = particle;
+		effect.angularVelocity = Math.PI / 10;
+		effect.particles = particlesCone;
+		effect.particlesCone = particlesCone;
+		effect.lengthGrow =  (float) height / particlesCone;
+		effect.radiusGrow = (float) radius / particlesCone;
+		effect.asynchronous = true;
+		effect.type = EffectType.INSTANT;
+		effect.iterations = 1;
+
+		return effect;
 	}
 
-	protected void renderSpike(LivingEntity target, ParticleEffect particle) {
-
+	private static ConeEffect createCone(EffectManager em, ParticleEffect particle, int particlesCone, double height, double radius, Color color) {
+		ConeEffect effect = createCone(em, particle, particlesCone, height, radius);
+		effect.color = color;
+		return effect;
 	}
 
-	private void renderSpike(LivingEntity target, ConeEffect effect) {
+	protected void renderSpike(Location location, double height, double radius, ParticleEffect particle, Color color) {
+		EffectManager em = new EffectManager(plugin);
+		renderSpike(location.clone(), em, createCone(em, particle, 120, height, radius, color), height);
+	}
 
+	protected void renderSpike(Location location, double height, double radius, ParticleEffect particle) {
+		EffectManager em = new EffectManager(plugin);
+		renderSpike(location.clone(), em, createCone(em, particle, 120, height, radius), height);
+	}
+
+	private void renderSpike(final Location location, final EffectManager em, final ConeEffect effect, final double height) {
+		new BukkitRunnable() {
+
+			private int life = 0;
+			private double increment = height / TICK_RISE_TIME;
+
+			@Override
+			public void run() {
+				location.setY(location.getY() + increment);
+				effect.setLocation(location);
+				effect.start();
+
+				if (++life >= TICK_RISE_TIME) {
+					cancel();
+					em.disposeOnTermination();
+				}
+			}
+
+		}.runTaskTimer(plugin, 0, 1);
 	}
 }
---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDamageSpike.java
@@ -0,0 +1,76 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.attributes.AttributeType;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import de.slikey.effectlib.util.ParticleEffect;
+import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.util.Vector;
+
+/**
+ * Created by Wally on 8/22/2015.
+ */
+public class SkillDamageSpike extends SkillBaseSpike {
+
+	private static final String KNOCKUP_STRENGTH = "knockup-strength";
+
+	public SkillDamageSpike(Heroes plugin) {
+		super(plugin, "DamageSpike");
+		setDescription("Summon a fire spike that damages foes for $1 and knocks them up into the air.");
+		setUsage("/skill damagespike");
+		setIdentifiers("skill damagespike");
+		setTypes(SkillType.DAMAGING, SkillType.ABILITY_PROPERTY_FIRE, SkillType.AGGRESSIVE);
+	}
+
+	@Override
+	public String getDescription(Hero hero) {
+		double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_TICK, 200d, false);
+		damage += SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_TICK_INCREASE_PER_INTELLECT, 2d, false) * hero.getAttributeValue(AttributeType.INTELLECT);
+
+		return getDescription().replace("$1", damage + "");
+	}
+
+	@Override
+	public ConfigurationSection getDefaultConfig() {
+		ConfigurationSection node = super.getDefaultConfig();
+
+		node.set(SkillSetting.DAMAGE.node(), 200d);
+		node.set(SkillSetting.DAMAGE_TICK_INCREASE_PER_INTELLECT.node(), 2d);
+		node.set(KNOCKUP_STRENGTH, 2d);
+
+		return node;
+	}
+
+	@Override
+	public SkillResult use(Hero hero, LivingEntity livingEntity, String[] strings) {
+		Player player = hero.getPlayer();
+
+		broadcastExecuteText(hero);
+
+		renderSpike(livingEntity.getLocation(), 2, 0.5, ParticleEffect.FLAME);
+
+		player.getWorld().playSound(hero.getPlayer().getLocation(), Sound.GHAST_FIREBALL, 5, 0.00001f);
+
+		if (damageCheck(player, livingEntity)) {
+			double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_TICK, 200d, false);
+			damage += SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_TICK_INCREASE_PER_INTELLECT, 2d, false) * hero.getAttributeValue(AttributeType.INTELLECT);
+
+			double knockUp = SkillConfigManager.getUseSetting(hero, this, KNOCKUP_STRENGTH, 2d, false);
+
+			damageEntity(livingEntity, player, damage, EntityDamageEvent.DamageCause.MAGIC);
+			livingEntity.setVelocity(new Vector(0, knockUp, 0));
+
+			return SkillResult.NORMAL;
+		} else {
+			return SkillResult.INVALID_TARGET;
+		}
+	}
+}
