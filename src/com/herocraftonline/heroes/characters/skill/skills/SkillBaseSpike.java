a38a853fe8ceb65913c1ab74c00228d54e1deebc
Walter Geisler <soren.endon@gmail.com>
2015-08-24 21:38:43 -0500

    Push improvments to base spike

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillBaseSpike.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillBaseSpike.java
@@ -1,14 +1,18 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Supplier;
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.characters.skill.TargettedSkill;
 import de.slikey.effectlib.EffectManager;
 import de.slikey.effectlib.EffectType;
 import de.slikey.effectlib.effect.ConeEffect;
 import de.slikey.effectlib.util.ParticleEffect;
-import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
@@ -18,8 +22,11 @@ public abstract class SkillBaseSpike extends TargettedSkill {
 
 	private static final Vector DOWN = new Vector(0, -1, 0);
 	private static final int PARTICLES_PER_HEIGHT = 10;
-	private static final double CONE_ANGULAR_VELOCITY = Math.PI / 5;
+	private static final int PARTICLES_PER_RADIUS = 10;
+	private static final double ANGULAR_VELOCITY_PI_DIVISOR_PER_RADIUS = 10;
 	private static final long TICK_RISE_TIME = 5;
+
+	protected static final double BLOCK_SPIKE_RADIUS = 0.5;
 
 	public SkillBaseSpike(Heroes plugin, String name) {
 		super(plugin, name);
@@ -29,8 +36,8 @@ public abstract class SkillBaseSpike extends TargettedSkill {
 		ConeEffect effect = new ConeEffect(em);
 
 		effect.particle = particle;
-		effect.angularVelocity = CONE_ANGULAR_VELOCITY;
-		effect.particles = (int) (height * PARTICLES_PER_HEIGHT);
+		effect.angularVelocity = Math.PI / (radius * ANGULAR_VELOCITY_PI_DIVISOR_PER_RADIUS);
+		effect.particles = 1 + (int) (height * PARTICLES_PER_HEIGHT) + (int) (radius * PARTICLES_PER_RADIUS);
 		effect.particlesCone = effect.particles;
 		effect.lengthGrow =  (float) height / effect.particles;
 		effect.radiusGrow = (float) radius / effect.particles;
@@ -47,7 +54,11 @@ public abstract class SkillBaseSpike extends TargettedSkill {
 		return effect;
 	}
 
-	protected void renderSpike(final Location location, final double height, final double radius, final ParticleEffect particle, final Color color) {
+	private abstract class ConeEffectProvider {
+		public abstract ConeEffect provide(EffectManager em, double scaledHeight);
+	}
+
+	private void renderSpike(final Location location, final double height, final ConeEffectProvider provider) {
 
 		location.setDirection(DOWN);
 
@@ -58,8 +69,11 @@ public abstract class SkillBaseSpike extends TargettedSkill {
 			@Override
 			public void run() {
 				EffectManager em = new EffectManager(plugin);
-				ConeEffect effect = createCone(em, particle, height, radius, color);
-				effect.setLocation(location.clone().add(0, height / TICK_RISE_TIME * life, 0));
+
+				double scaledHeight = (height / TICK_RISE_TIME) * life;
+				ConeEffect effect = provider.provide(em, scaledHeight);
+				effect.setLocation(location.clone().add(0, scaledHeight, 0));
+
 				effect.start();
 				em.disposeOnTermination();
 
@@ -70,26 +84,29 @@ public abstract class SkillBaseSpike extends TargettedSkill {
 		}.runTaskTimer(plugin, 0, 1);
 	}
 
-	protected void renderSpike(final Location location, final double height, final double radius, final ParticleEffect particle) {
-
-		location.setDirection(DOWN);
-
-		new BukkitRunnable() {
-
-			private int life = 0;
-
+	protected void renderSpike(Location location, double height, final double radius, final ParticleEffect particle, final Color color) {
+		renderSpike(location.clone(), height, new ConeEffectProvider() {
 			@Override
-			public void run() {
-				EffectManager em = new EffectManager(plugin);
-				ConeEffect effect = createCone(em, particle, height / TICK_RISE_TIME * life, radius);
-				effect.setLocation(location.clone().add(0, height / TICK_RISE_TIME * life, 0));
-				effect.start();
-				em.disposeOnTermination();
-
-				if (++life > TICK_RISE_TIME) {
-					cancel();
-				}
+			public ConeEffect provide(EffectManager em, double scaledHeight) {
+				return createCone(em, particle, scaledHeight, radius, color);
 			}
-		}.runTaskTimer(plugin, 0, 1);
+		});
+	}
+
+	protected void renderSpike(Location location, double height, final double radius, final ParticleEffect particle) {
+		renderSpike(location.clone(), height, new ConeEffectProvider() {
+			@Override
+			public ConeEffect provide(EffectManager em, double scaledHeight) {
+				return createCone(em, particle, scaledHeight, radius);
+			}
+		});
+	}
+
+	protected void renderSpikeOnBlock(Block block, double height, ParticleEffect particle, Color color) {
+		renderSpike(block.getLocation().add(0.5, 1, 0.5), height, BLOCK_SPIKE_RADIUS, particle, color);
+	}
+
+	protected void renderSpikeOnBlock(Block block, double height, ParticleEffect particle) {
+		renderSpike(block.getLocation().add(0.5, 1, 0.5), height, BLOCK_SPIKE_RADIUS, particle);
 	}
 }
