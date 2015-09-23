72648a3ec1c25fe049910b9d9281a73eacbaa5f9
0xNaomi <dkabot@dkabot.com>
2015-09-23 00:40:50 -0400

    Apparently not being in a town is a separate exception.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillTownSpawn.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillTownSpawn.java
@@ -4,20 +4,37 @@ import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.DelayedSkill;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.chat.ChatComponents;
+import com.herocraftonline.heroes.util.Messaging;
 import com.palmergames.bukkit.towny.TownyMessaging;
 import com.palmergames.bukkit.towny.command.TownCommand;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.exceptions.TownyException;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 
 public class SkillTownSpawn extends ActiveSkill {
 
     public SkillTownSpawn(Heroes plugin) {
-        super(plugin, "townspawn");
+        super(plugin, "TownSpawn");
         setDescription("Teleport to your town's spawn.");
         setUsage("/skill townspawn");
         setIdentifiers("skill townspawn");
         setArgumentRange(0, 0);
         setTypes(SkillType.SILENCEABLE, SkillType.TELEPORTING);
+    }
+
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set(SkillSetting.NO_COMBAT_USE.node(), true);
+        node.set(SkillSetting.DELAY.node(), 10000);
+
+        return node;
     }
 
     @Override
@@ -27,14 +44,23 @@ public class SkillTownSpawn extends ActiveSkill {
 
     @Override
     public SkillResult use(Hero hero, String[] args) {
+        Player player = hero.getPlayer();
+
         try {
             TownCommand.townSpawn(hero.getPlayer(), args, false);
         }
-        catch (TownyException e) {
-            TownyMessaging.sendErrorMsg(hero.getPlayer(), e.getMessage());
+        catch (NotRegisteredException e) {
+            Messaging.send(player, "You don't belong to a Town!");
+            return SkillResult.FAIL;
         }
+        catch (TownyException e) {
+            Messaging.send(player, e.getMessage());
+            return SkillResult.FAIL;
+        }
+
         return SkillResult.NORMAL;
     }
+
 
 
 }
