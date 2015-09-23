afb7b9affa049cbf704becb192c905a2ecf5b851
0xNaomi <dkabot@dkabot.com>
2015-09-22 23:33:41 -0400

    Initial testing of /town spawn skill.

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillTownSpawn.java
@@ -0,0 +1,39 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.ActiveSkill;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.palmergames.bukkit.towny.TownyMessaging;
+import com.palmergames.bukkit.towny.command.TownCommand;
+import com.palmergames.bukkit.towny.exceptions.TownyException;
+
+public class SkillTownSpawn extends ActiveSkill {
+
+    public SkillTownSpawn(Heroes plugin) {
+        super(plugin, "townspawn");
+        setDescription("Teleport to your town's spawn.");
+        setUsage("/skill townspawn");
+        setIdentifiers("skill townspawn");
+        setArgumentRange(0, 0);
+        setTypes(SkillType.SILENCEABLE, SkillType.TELEPORTING);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        return getDescription();
+    }
+
+    @Override
+    public SkillResult use(Hero hero, String[] args) {
+        try {
+            TownCommand.townSpawn(hero.getPlayer(), args, false);
+        }
+        catch (TownyException e) {
+            TownyMessaging.sendErrorMsg(hero.getPlayer(), e.getMessage());
+        }
+    }
+
+
+}
