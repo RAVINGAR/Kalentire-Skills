91a1860d3180beb63d80d1a1494b3b4bd2a65d28
0xNaomi <dkabot@dkabot.com>
2015-09-28 19:56:32 -0400

    Try to make HarmTouch slow and reduce healing

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillHarmtouch.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillHarmtouch.java
@@ -1,32 +1,40 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
+import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
+import com.herocraftonline.heroes.characters.Monster;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.effects.common.SlowEffect;
+import com.herocraftonline.heroes.characters.skill.*;
+import com.herocraftonline.heroes.util.Messaging;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Sound;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.attributes.AttributeType;
 import com.herocraftonline.heroes.characters.Hero;
-import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
-import com.herocraftonline.heroes.characters.skill.SkillSetting;
-import com.herocraftonline.heroes.characters.skill.SkillType;
-import com.herocraftonline.heroes.characters.skill.TargettedSkill;
-import com.herocraftonline.heroes.characters.skill.VisualEffect;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 
 public class SkillHarmtouch extends TargettedSkill {
 
+    private String applyText;
+    private String expireText;
+
     public SkillHarmtouch(Heroes plugin) {
         super(plugin, "Harmtouch");
-        setDescription("You deal $1 dark damage to the target.");
+        setDescription("You deal $1 dark damage to the target, reducing their damage by $2% for $3 seconds.");
         setUsage("/skill harmtouch");
         setArgumentRange(0, 0);
         setIdentifiers("skill harmtouch");
-        setTypes(SkillType.ABILITY_PROPERTY_DARK, SkillType.SILENCEABLE, SkillType.DAMAGING, SkillType.AGGRESSIVE);
+        setTypes(SkillType.ABILITY_PROPERTY_DARK, SkillType.SILENCEABLE, SkillType.DAMAGING, SkillType.AGGRESSIVE, SkillType.MOVEMENT_SLOWING);
     }
 
     @Override
@@ -35,7 +43,17 @@ public class SkillHarmtouch extends TargettedSkill {
         double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 1.75, false);
         damage += (int) (damageIncrease * hero.getAttributeValue(AttributeType.INTELLECT));
 
-        return getDescription().replace("$1", damage + "");
+        double heal = 1 - SkillConfigManager.getUseSetting(hero, this, "heal-multiplier", .5, true);
+        int duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 12000, false);
+
+        return getDescription().replace("$1", damage + "").replace("$2", heal * 100 + "").replace("$3", duration / 1000 + "");
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        applyText = SkillConfigManager.getRaw(this, SkillSetting.APPLY_TEXT, "%target% has been harmfully touched by %hero%!").replace("%target%", "$1").replace("$2", "%hero%");
+        expireText = SkillConfigManager.getRaw(this, SkillSetting.EXPIRE_TEXT, "%target% has recovered from their harmful touch!").replace("%target%", "$1");
     }
 
     @Override
@@ -47,6 +65,11 @@ public class SkillHarmtouch extends TargettedSkill {
         node.set(SkillSetting.DAMAGE_INCREASE_PER_INTELLECT.node(), 1.75);
         node.set(SkillSetting.HEALTH_COST.node(), 75);
 
+        node.set(SkillSetting.DURATION.node(), 12000);
+        node.set("heal-multiplier", .5);
+        node.set("slow-level", 2);
+        node.set(SkillSetting.APPLY_TEXT.node(), "%target% has been harmfully touched by %hero%!");
+        node.set(SkillSetting.EXPIRE_TEXT.node(), "%target% has recovered from their harmful touch!");
         return node;
     }
 
@@ -62,6 +85,11 @@ public class SkillHarmtouch extends TargettedSkill {
         damageEntity(target, player, damage, DamageCause.MAGIC);
         broadcastExecuteText(hero, target);
 
+        long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 12000, false);
+        int amplifier = SkillConfigManager.getUseSetting(hero, this, "slow-level", 2, false);
+        double healMultiplier = SkillConfigManager.getUseSetting(hero, this, "heal-multiplier", 0.5, true);
+        plugin.getCharacterManager().getCharacter(target).addEffect(new HarmTouch(this, player, duration, amplifier, healMultiplier));
+
 
         hero.getPlayer().getWorld().playSound(hero.getPlayer().getLocation(), Sound.WITHER_SPAWN , 0.8F, 1.0F);
         hero.getPlayer().getWorld().spigot().playEffect(target.getLocation(), org.bukkit.Effect.EXPLOSION_LARGE, 0, 0, 0.3F, 1.2F, 0.3F, 0.0F, 15, 16);
@@ -69,5 +97,69 @@ public class SkillHarmtouch extends TargettedSkill {
         hero.getPlayer().getWorld().spigot().playEffect(target.getLocation(), org.bukkit.Effect.SPELL, 0, 0, 0.3F, 1.2F, 0.3F, 0.0F, 25, 16);
 
         return SkillResult.NORMAL;
+    }
+
+    public class HarmTouch extends SlowEffect {
+
+        private final double healMultiplier;
+
+        public HarmTouch(Skill skill, Player applier, long duration, int amplifier, double healMultiplier) {
+            super(skill, "HarmTouch", applier, duration, amplifier);
+            this.healMultiplier = healMultiplier;
+
+            types.add(EffectType.SLOW);
+            types.add(EffectType.HARMFUL);
+        }
+
+        @Override
+        public void applyToMonster(Monster monster) {
+            super.applyToMonster(monster);
+            broadcast(monster.getEntity().getLocation(), "    " + applyText, Messaging.getLivingEntityName(monster), applier.getName());
+        }
+
+        @Override
+        public void applyToHero(Hero hero) {
+            super.applyToHero(hero);
+            Player player = hero.getPlayer();
+            broadcast(player.getLocation(), "    " + applyText, player.getName(), applier.getName());
+        }
+
+        @Override
+        public void removeFromMonster(Monster monster) {
+            super.removeFromMonster(monster);
+            broadcast(monster.getEntity().getLocation(), "    " + expireText, Messaging.getLivingEntityName(monster));
+        }
+
+        @Override
+        public void removeFromHero(Hero hero) {
+            super.removeFromHero(hero);
+
+            Player player = hero.getPlayer();
+            broadcast(player.getLocation(), "    " + expireText, player.getName());
+        }
+    }
+
+    public class SkillEntityListener implements Listener {
+
+        @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+        public void onEntityRegainHealth(EntityRegainHealthEvent event) {
+            if (!(event.getEntity() instanceof Player)) {
+                return;
+            }
+
+            Hero hero = plugin.getCharacterManager().getHero((Player) event.getEntity());
+            if (hero.hasEffect("HarmTouch")) {
+                HarmTouch mEffect = (HarmTouch) hero.getEffect("HarmTouch");
+                event.setAmount((event.getAmount() * mEffect.healMultiplier));
+            }
+        }
+
+        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+        public void onHeroRegainHealth(HeroRegainHealthEvent event) {
+            if (event.getHero().hasEffect("HarmTouch")) {
+                HarmTouch mEffect = (HarmTouch) event.getHero().getEffect("HarmTouch");
+                event.setAmount((event.getAmount() * mEffect.healMultiplier));
+            }
+        }
     }
 }
