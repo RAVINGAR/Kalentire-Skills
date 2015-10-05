ac4b0e563ed63a1bd5d95d3338ef1b4daa027192
Walter Geisler <soren.endon@gmail.com>
2015-10-05 00:29:25 -0500

    Declare starts to 4 block wave skills

---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDivineRuination.java
@@ -0,0 +1,45 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class SkillDivineRuination extends SkillBaseBlockWave {
+
+	public SkillDivineRuination(Heroes plugin) {
+		super(plugin, "DivineRuination");
+		setDescription("");
+		setUsage("/skill " + getName().toLowerCase());
+		setIdentifiers("skill " + getName().toLowerCase());
+		setArgumentRange(0, 0);
+	}
+
+	@Override
+	public String getDescription(Hero hero) {
+		return getDescription();
+	}
+
+	@Override
+	public ConfigurationSection getDefaultConfig() {
+		ConfigurationSection node = super.getDefaultConfig();
+
+		node.set(SkillSetting.RADIUS.node(), 5);
+		node.set(HEIGHT_NODE, 3);
+		node.set(DEPTH_NODE, 5);
+		node.set(EXPANSION_RATE_NODE, 1);
+		node.set(LAUNCH_FORCE_NODE, 0.2);
+		node.set(HIT_LIMIT_NODE, 1);
+
+		node.set(SkillSetting.DAMAGE.node(), 100d);
+		node.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), 1);
+
+		return node;
+	}
+
+	@Override
+	public SkillResult use(Hero hero, String[] strings) {
+		return null;
+	}
+}
---
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillSeismicAssault.java
@@ -0,0 +1,46 @@
+package com.herocraftonline.heroes.characters.skill.skills;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class SkillSeismicAssault extends SkillBaseBlockWave {
+
+	public SkillSeismicAssault(Heroes plugin) {
+		super(plugin, "SeismicAssault");
+		setDescription("");
+		setUsage("/skill " + getName().toLowerCase());
+		setIdentifiers("skill " + getName().toLowerCase());
+		setArgumentRange(0, 0);
+	}
+
+	@Override
+	public String getDescription(Hero hero) {
+		return getDescription();
+	}
+
+	@Override
+	public ConfigurationSection getDefaultConfig() {
+		ConfigurationSection node = super.getDefaultConfig();
+
+		node.set(SkillSetting.RADIUS.node(), 8);
+		node.set(HEIGHT_NODE, 3);
+		node.set(DEPTH_NODE, 5);
+		node.set(EXPANSION_RATE_NODE, 1);
+		node.set(WAVE_ARC_NODE, 60d);
+		node.set(LAUNCH_FORCE_NODE, 0.2);
+		node.set(HIT_LIMIT_NODE, 1);
+
+		node.set(SkillSetting.DAMAGE.node(), 100d);
+		node.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), 1);
+
+		return node;
+	}
+
+	@Override
+	public SkillResult use(Hero hero, String[] strings) {
+		return null;
+	}
+}
