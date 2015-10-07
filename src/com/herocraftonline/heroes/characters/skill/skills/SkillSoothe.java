db3b2812c434501b871aa483b6a585349057d6e3
0xNaomi <dkabot@dkabot.com>
2015-10-07 09:51:18 -0400

    Update Soothe to be a SkillBaseHeal Skill

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillSoothe.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillSoothe.java
@@ -1,6 +1,7 @@
 package com.herocraftonline.heroes.characters.skill.skills;
 
 import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -19,7 +20,7 @@ import com.herocraftonline.heroes.characters.skill.TargettedSkill;
 import com.herocraftonline.heroes.util.Messaging;
 import com.herocraftonline.heroes.util.Util;
 
-public class SkillSoothe extends TargettedSkill {
+public class SkillSoothe extends SkillBaseHeal {
 
     public SkillSoothe(Heroes plugin) {
         super(plugin, "Soothe");
@@ -28,18 +29,6 @@ public class SkillSoothe extends TargettedSkill {
         setArgumentRange(0, 1);
         setIdentifiers("skill soothe");
         setTypes(SkillType.ABILITY_PROPERTY_LIGHT, SkillType.DISPELLING, SkillType.HEALING, SkillType.SILENCEABLE);
-    }
-
-    @Override
-    public String getDescription(Hero hero) {
-        double healing = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING.node(), 125, false);
-        double healingIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING_INCREASE_PER_WISDOM.node(), 2.0, false);
-        healing += (hero.getAttributeValue(AttributeType.WISDOM) * healingIncrease);
-
-        String formattedHealing = Util.decFormat.format(healing);
-        String formattedSelfHealing = Util.decFormat.format(healing * Heroes.properties.selfHeal);
-
-        return getDescription().replace("$1", formattedHealing).replace("$2", formattedSelfHealing);
     }
 
     @Override
@@ -54,50 +43,23 @@ public class SkillSoothe extends TargettedSkill {
     }
 
     @Override
-    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
-        Player player = hero.getPlayer();
-        if (!(target instanceof Player)) {
-            return SkillResult.INVALID_TARGET;
-        }
+    protected void applySoundEffects(World world, LivingEntity target) {
+        world.playSound(target.getLocation(), Sound.CHICKEN_EGG_POP, 0.5F, 0.01F);
+    }
 
-        Hero targetHero = plugin.getCharacterManager().getHero((Player) target);
-        double healing = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING, 125, false);
-        double healingIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING_INCREASE_PER_WISDOM, 2.0, false);
-        healing += (hero.getAttributeValue(AttributeType.WISDOM) * healingIncrease);
+    @Override
+    protected void applyParticleEffects(World world, LivingEntity target) {
+        // Original skill had no particle effects, placeholder to keep it the same
+    }
 
-        double targetHealth = target.getHealth();
-        if (targetHealth >= target.getMaxHealth()) {
-            if (player.equals(targetHero.getPlayer())) {
-                Messaging.send(player, "You are already at full health.");
-            }
-            else {
-                Messaging.send(player, "Target is already fully healed.");
-            }
-            return SkillResult.INVALID_TARGET_NO_MSG;
-        }
-
-        HeroRegainHealthEvent hrhEvent = new HeroRegainHealthEvent(targetHero, healing, this, hero);
-        plugin.getServer().getPluginManager().callEvent(hrhEvent);
-        if (hrhEvent.isCancelled()) {
-            Messaging.send(player, "Unable to heal the target at this time!");
-            return SkillResult.CANCELLED;
-        }
-
-        broadcastExecuteText(hero, target);
-
-        targetHero.heal(hrhEvent.getAmount());
-
-        // Soothe cures WITHER!
-        for (Effect effect : targetHero.getEffects()) {
+    @Override
+    protected void removeEffects(Hero hero) {
+        for (Effect effect : hero.getEffects()) {
             if (effect.isType(EffectType.DISPELLABLE) && effect.isType(EffectType.HARMFUL)) {
                 if (effect.isType(EffectType.WITHER)) {
-                    targetHero.removeEffect(effect);
+                    hero.removeEffect(effect);
                 }
             }
         }
-
-        player.getWorld().playSound(player.getLocation(), Sound.CHICKEN_EGG_POP, 0.5F, 0.01F);
-
-        return SkillResult.NORMAL;
     }
 }
