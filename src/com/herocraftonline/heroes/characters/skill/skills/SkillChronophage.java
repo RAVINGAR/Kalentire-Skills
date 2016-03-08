00b1f5140083524da8e3550bd10ea9b08bb2f6cb
0xNaomi <dkabot@dkabot.com>
2016-03-07 19:14:49 -0500

    Switch Chronophage back to damage

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillChronophage.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillChronophage.java
@@ -11,36 +11,32 @@ import com.herocraftonline.heroes.util.Util;
 import de.slikey.effectlib.util.ParticleEffect;
 import org.bukkit.Color;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class SkillChronophage extends SkillBaseMarkedTeleport {
 
-    private static final String HEALING_PERCENTAGE_NODE = "healing-percentage";
-    private static final String HEALING_PERCENTAGE_PER_WISDOM_NODE = "healing-percentage-per-wisdom";
-
     public SkillChronophage(Heroes plugin) {
-        super(plugin, "Chronophage", true, new EffectType[]{
-                EffectType.HEALING,
-                EffectType.BENEFICIAL,
-        }, ParticleEffect.REDSTONE, new Color[]{
+        super(plugin, "Chronophage", false, new EffectType[] {
+                EffectType.DAMAGING,
+                EffectType.HARMFUL
+        }, ParticleEffect.REDSTONE, new Color[] {
                 Color.PURPLE,
-                Color.BLUE
+                Color.BLACK
         });
-        // TODO Dscription change
-        setDescription("Mark your current position in time for the next $1 seconds. At any point during that time you may re activate the skill to teleport" +
-                " your self back to that location healing you for an amount starting at $3 ($2% of max health) and decaying towards 0 as the skills duration reaches end. If you do not" +
-                " re activate the skill within the duration no healing is applied and no teleport occurs. $4 $5");
+        setDescription("Mark your target's current position in time for the next $1 seconds. At any point during that time you may re activate the skill to teleport" +
+                " the target back to that location dealing an amount of damage starting at $2 and decaying towards 0 as the skills duration reaches end. If you do not" +
+                " re activate the skill within the duration no damage is dealt and no teleport occurs. $4 $5");
         setUsage("/skill chronophage");
         setIdentifiers("skill chronophage");
 
-        // TODO type edit
-        setTypes(SkillType.HEALING, SkillType.TELEPORTING);
+        setTypes(SkillType.AGGRESSIVE, SkillType.DAMAGING, SkillType.NO_SELF_TARGETTING, SkillType.TELEPORTING);
     }
 
     @Override
     public String getDescription(Hero hero) {
-        double healing = SkillConfigManager.getUseSetting(hero, this, HEALING_PERCENTAGE_NODE, 0.25d, false);
-        double healingIncrease = SkillConfigManager.getUseSetting(hero, this, HEALING_PERCENTAGE_PER_WISDOM_NODE, 0.005d, false);
-        healing += hero.getAttributeValue(AttributeType.WISDOM) * healingIncrease;
+        double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 250d, false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 1d, false);
+        damage += hero.getAttributeValue(AttributeType.INTELLECT) * damageIncrease;
 
         double totalDuration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 10000, false);
 
@@ -49,8 +45,7 @@ public class SkillChronophage extends SkillBaseMarkedTeleport {
 
         return getDescription()
                 .replace("$1", Util.decFormat.format(totalDuration / 1000))
-                .replace("$2", Util.largeDecFormat.format(healing * 100))
-                .replace("$3", Util.decFormat.format(hero.getPlayer().getMaxHealth() * healing))
+                .replace("$2", Util.decFormat.format(damage))
                 .replace("$4", mana > 0 ? "Mana: " + mana : "")
                 .replace("$5", cooldown > 0 ? "C: " + Util.decFormat.format((double) cooldown / 1000) : "");
     }
@@ -60,8 +55,8 @@ public class SkillChronophage extends SkillBaseMarkedTeleport {
         ConfigurationSection node = super.getDefaultConfig();
 
         node.set(SkillSetting.DURATION.node(), 10000);
-        node.set(HEALING_PERCENTAGE_NODE, 0.25d);
-        node.set(HEALING_PERCENTAGE_PER_WISDOM_NODE, 0.005d);
+        node.set(SkillSetting.DAMAGE.node(), 250d);
+        node.set(SkillSetting.DAMAGE_INCREASE_PER_INTELLECT.node(), 1);
 
         node.set(PRESERVE_LOOK_DIRECTION_NODE, true);
         node.set(PRESERVE_VELOCITY_NODE, true);
@@ -70,20 +65,20 @@ public class SkillChronophage extends SkillBaseMarkedTeleport {
     }
 
     @Override
-    protected void onMarkerActivate(SkillBaseMarkedTeleport.Marker marker, long activateTime) {
-        double healing = SkillConfigManager.getUseSetting(marker.getHero(), this, HEALING_PERCENTAGE_NODE, 0.25d, false);
-        double healingIncrease = SkillConfigManager.getUseSetting(marker.getHero(), this, HEALING_PERCENTAGE_PER_WISDOM_NODE, 0.005d, false);
-        healing += marker.getHero().getAttributeValue(AttributeType.WISDOM) * healingIncrease;
+    protected void onMarkerActivate(Marker marker, long activateTime) {
+        if (damageCheck(marker.getHero().getPlayer(), marker.getTarget().getEntity())) {
+            double damage = SkillConfigManager.getUseSetting(marker.getHero(), this, SkillSetting.DAMAGE, 250d, false);
+            double damageIncrease = SkillConfigManager.getUseSetting(marker.getHero(), this, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 1d, false);
+            damage += marker.getHero().getAttributeValue(AttributeType.INTELLECT) * damageIncrease;
+            long reCastDelay = SkillConfigManager.getUseSetting(marker.getHero(), this, RE_CAST_DELAY_NODE, 0, false);
 
-        double maxHeal = marker.getTarget().getEntity().getMaxHealth() * healing;
-        long reCastDelay = SkillConfigManager.getUseSetting(marker.getHero(), this, RE_CAST_DELAY_NODE, 0, false);
+            double totalDuration = SkillConfigManager.getUseSetting(marker.getHero(), this, SkillSetting.DURATION, 10000, false);
+            double damageScale = 1 - ((activateTime - marker.getCreateTime() - reCastDelay) / (totalDuration - reCastDelay));
+            if (damageScale < 0) {
+                damageScale = 0;
+            }
 
-        double totalDuration = SkillConfigManager.getUseSetting(marker.getHero(), this, SkillSetting.DURATION, 10000, false);
-        double healScale = 1 - ((activateTime - marker.getCreateTime() + reCastDelay) / (totalDuration - reCastDelay));
-        if (healScale < 0) {
-            healScale = 0;
+            damageEntity(marker.getTarget().getEntity(), marker.getHero().getPlayer(), damage * damageScale, EntityDamageEvent.DamageCause.MAGIC, false);
         }
-
-        ((Hero) marker.getTarget()).heal(maxHeal * healScale);
     }
 }
