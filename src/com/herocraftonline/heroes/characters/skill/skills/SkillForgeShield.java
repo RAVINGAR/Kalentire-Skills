286d09c868974b92c7b49fb7721f4f00d047534f
rramos <rodney@bighatchet.com>
2016-07-10 11:07:02 -0700

    lower case for consistency

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeShield.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillForgeShield.java
@@ -9,9 +9,9 @@ public class SkillForgeShield extends SkillBaseForgeItem {
     public SkillForgeShield(Heroes plugin) {
         super(plugin, "ForgeShield");
         setDescription("You forge a mighty shield!!");
-        setUsage("/skill FORGESHIELD");
+        setUsage("/skill forgeshield");
         setArgumentRange(0, 0);
-        setIdentifiers("skill FORGESHIELD");
+        setIdentifiers("skill forgeshield");
         setTypes(SkillType.ITEM_CREATION);
         defaultAmount = 1;
         deafultItem = Material.SHIELD;
