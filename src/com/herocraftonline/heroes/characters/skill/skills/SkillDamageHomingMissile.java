8c91fcd11284c7ffd0c9443e41d47ff4faed7b77
Walter Geisler <soren.endon@gmail.com>
2016-08-10 12:03:47 -0500

    Moved homing missile stuff back to `skills` because Kainzo is rearranging stuff.

--- src/com/herocraftonline/heroes/characters/skill/skills/SkillDamageHomingMissile.java
+++ src/com/herocraftonline/heroes/characters/skill/skills/SkillDamageHomingMissile.java
@@ -1,4 +1,4 @@
-package com.herocraftonline.heroes.characters.skill.public1;
+package com.herocraftonline.heroes.characters.skill.skills;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.SkillResult;
