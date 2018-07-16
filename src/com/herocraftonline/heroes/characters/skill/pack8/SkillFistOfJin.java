7af3b0fa556694638579b91220520217b8fd5e96
MysticMight <benthegreat@y7mail.com>
2018-07-16 23:12:35 +1000

    add heal scaling to fist of jin

--- src/com/herocraftonline/heroes/characters/skill/pack8/SkillFistOfJin.java
+++ src/com/herocraftonline/heroes/characters/skill/pack8/SkillFistOfJin.java
@@ -5,6 +5,8 @@ import com.herocraftonline.heroes.api.events.HeroRegainHealthEvent;
 import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
 import com.herocraftonline.heroes.attributes.AttributeType;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.classes.scaling.ExpressionScaling;
+import com.herocraftonline.heroes.characters.classes.scaling.Scaling;
 import com.herocraftonline.heroes.characters.effects.EffectType;
 import com.herocraftonline.heroes.characters.effects.ExpirableEffect;
 import com.herocraftonline.heroes.characters.skill.*;
@@ -41,7 +43,9 @@ public class SkillFistOfJin extends PassiveSkill {
         double cdDuration = Util.formatDouble(SkillConfigManager.getUseSetting(hero, this, "healing-internal-cooldown", 1000.0, false) / 1000.0);
 
         double selfHeal = SkillConfigManager.getUseSetting(hero, this, "heal-per-hit-self", 8, false);
+        selfHeal = getScaledHealing(hero, selfHeal);
         double partyHeal = SkillConfigManager.getUseSetting(hero, this, "heal-per-hit-party", 3, false);
+        partyHeal = getScaledHealing(hero, partyHeal);
 
         double healingIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING_INCREASE_PER_WISDOM, 0.15, false);
         double calculatedIncrease = (hero.getAttributeValue(AttributeType.WISDOM) * healingIncrease);
@@ -67,6 +71,10 @@ public class SkillFistOfJin extends PassiveSkill {
         node.set(SkillSetting.HEALING_INCREASE_PER_WISDOM.node(), 0.15);
         node.set(SkillSetting.RADIUS.node(), 6);
         node.set(SkillSetting.RADIUS_INCREASE_PER_WISDOM.node(), 0.1);
+
+        // heal scaling parameters for use in children skills
+        node.set(SkillSetting.IS_SCALED_HEALING.node(), false);
+        node.set(SkillSetting.HEALING_SCALE_EXPRESSION.node(), "1");
 
         List<String> weaponList = new ArrayList<String>(5);
         weaponList.add("AIR");
@@ -117,7 +125,9 @@ public class SkillFistOfJin extends PassiveSkill {
             int wisdom = hero.getAttributeValue(AttributeType.WISDOM);
 
             double selfHeal = SkillConfigManager.getUseSetting(hero, skill, "heal-per-hit-self", 8, false);
+            selfHeal = getScaledHealing(hero, selfHeal);
             double partyHeal = SkillConfigManager.getUseSetting(hero, skill, "heal-per-hit-party", 3, false);
+            partyHeal = getScaledHealing(hero, partyHeal);
 
             double healingIncrease = SkillConfigManager.getUseSetting(hero, skill, SkillSetting.HEALING_INCREASE_PER_WISDOM, 0.15, false);
             double calculatedIncrease = wisdom * healingIncrease;
@@ -183,5 +193,22 @@ public class SkillFistOfJin extends PassiveSkill {
         public CooldownEffect(Skill skill, Player applier, long duration) {
             super(skill, "FistOfJinCooldownEffect", applier, duration);
         }
+    }
+
+    /**
+     * @param hero the {@link Hero} using the skill
+     * @param healing the base healing rate to scale
+     * @return if health scaling enabled with a scaling expression return healing * expression scaling, otherwise return base healing.
+     */
+    public double getScaledHealing(Hero hero, double healing) {
+        boolean scaledHealing = SkillConfigManager.getUseSetting(hero, this, SkillSetting.IS_SCALED_HEALING.node(), false);
+        if (scaledHealing){
+            String expression = SkillConfigManager.getUseSetting(hero, this, SkillSetting.HEALING_SCALE_EXPRESSION.node(),"1");
+            if (!expression.equals("1")) {
+                Scaling healingScaling = new ExpressionScaling(hero.getHeroClass(), expression);
+                healing = healing * healingScaling.getScaled(hero);
+            }
+        }
+        return healing;
     }
 }
