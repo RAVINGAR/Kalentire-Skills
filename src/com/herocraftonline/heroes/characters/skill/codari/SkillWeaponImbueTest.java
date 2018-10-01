3559a6a02b58f2ed5613bba51aa32d7602bdbf78
Walter Geisler <soren.endon@gmail.com>
2018-10-01 18:31:53 -0500

    Trying to test stacking effects.

--- src/com/herocraftonline/heroes/characters/skill/codari/SkillWeaponImbueTest.java
+++ src/com/herocraftonline/heroes/characters/skill/codari/SkillWeaponImbueTest.java
@@ -2,8 +2,13 @@ package com.herocraftonline.heroes.characters.skill.codari;
 
 import com.herocraftonline.heroes.Heroes;
 import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
+import com.herocraftonline.heroes.characters.CharacterTemplate;
 import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.Monster;
+import com.herocraftonline.heroes.characters.effects.PeriodicStackingEffect;
+import com.herocraftonline.heroes.characters.skill.Skill;
 import com.herocraftonline.heroes.characters.skill.skills.SkillBaseWeaponImbue;
+import org.bukkit.entity.LivingEntity;
 
 public class SkillWeaponImbueTest extends SkillBaseWeaponImbue {
 
@@ -23,6 +28,34 @@ public class SkillWeaponImbueTest extends SkillBaseWeaponImbue {
 
     @Override
     protected void apply(Hero hero, String[] strings, WeaponDamageEvent weaponDamageEvent) {
-        hero.getPlayer().sendMessage("APPLIED!");
+
+        final String NAME = "Test Stacking Effect";
+
+        CharacterTemplate target = plugin.getCharacterManager().getCharacter((LivingEntity) weaponDamageEvent.getEntity());
+
+        TestStackingEffect effect = (TestStackingEffect) target.getEffect(NAME);
+        if (effect == null) {
+            effect = new TestStackingEffect(this, NAME);
+            target.addEffect(effect);
+        }
+
+        effect.addStack(hero, 10000, hero.getPlayer(), this);
+    }
+
+    public class TestStackingEffect extends PeriodicStackingEffect {
+
+        public TestStackingEffect(Skill skill, String name) {
+            super(skill, name, 5, 500);
+        }
+
+        @Override
+        public void tickMonster(Monster monster) {
+
+        }
+
+        @Override
+        public void tickHero(Hero hero) {
+            hero.getPlayer().sendMessage("You have " + getStackCount(hero) + " stacks");
+        }
     }
 }
