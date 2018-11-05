7a898a1262d3dc68da1ab148f5ca5026ded2d615
Walter Geisler <soren.endon@gmail.com>
2018-11-05 13:30:13 -0600

    Progress on roll skill

--- src/com/herocraftonline/heroes/characters/skill/codari/SkillTheySeeMeRolling.java
+++ src/com/herocraftonline/heroes/characters/skill/codari/SkillTheySeeMeRolling.java
@@ -5,7 +5,9 @@ import com.herocraftonline.heroes.api.SkillResult;
 import com.herocraftonline.heroes.characters.Hero;
 import com.herocraftonline.heroes.characters.skill.ActiveSkill;
 import com.herocraftonline.heroes.characters.skill.RecastData;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,6 +22,21 @@ import java.util.Map;
 import java.util.UUID;
 
 public class SkillTheySeeMeRolling extends ActiveSkill {
+
+    private static final String FORWARD_RECAST_NAME = "Forward";
+    private static final String SIDEWAYS_RECAST_NAME = "Sideways";
+    private static final String BACKWARDS_RECAST_NAME = "Backwards";
+
+    private static final int MIN_RECAST_DURATION = 500;
+
+    private static final String FORWARDS_RECAST_DURATION_NODE = "forwards-recast-duration";
+    private static final int DEFAULT_FORWARDS_RECAST_DURATION = 1500;
+
+    private static final String SIDEWAYS_RECAST_DURATION_NODE = "sideways-recast-duration";
+    private static final int DEFAULT_SIDEWAYS_RECAST_DURATION = 1500;
+
+    private static final String BACKWARDS_RECAST_DURATION_NODE = "backwards-recast-duration";
+    private static final int DEFAULT_BACKWARDS_RECAST_DURATION = 1500;
 
     public SkillTheySeeMeRolling(Heroes plugin) {
         super(plugin, "TheySeeMeRolling");
@@ -37,6 +54,18 @@ public class SkillTheySeeMeRolling extends ActiveSkill {
     }
 
     @Override
+    public ConfigurationSection getDefaultConfig() {
+
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set(FORWARDS_RECAST_DURATION_NODE, DEFAULT_FORWARDS_RECAST_DURATION);
+        node.set(SIDEWAYS_RECAST_DURATION_NODE, DEFAULT_SIDEWAYS_RECAST_DURATION);
+        node.set(BACKWARDS_RECAST_DURATION_NODE, DEFAULT_BACKWARDS_RECAST_DURATION);
+
+        return node;
+    }
+
+    @Override
     public String getDescription(Hero hero) {
         return getDescription();
     }
@@ -51,8 +80,10 @@ public class SkillTheySeeMeRolling extends ActiveSkill {
             return SkillResult.CANCELLED;
         }
 
-        RecastData recastData = new RecastData("Roll");
-        recastData.setNeverReady();
+        {
+            RecastData recastData = new RecastData("Roll");
+            recastData.setNeverReady();
+        }
 
         final Vector origin = player.getLocation().toVector();
 
@@ -70,16 +101,27 @@ public class SkillTheySeeMeRolling extends ActiveSkill {
 
             double yawDifference = Math.min(360 - Math.abs(yawDirection - yawMovement), Math.abs(yawDirection - yawMovement));
 
+            RecastData recastData;
+            int recastDuration;
+
             if (yawDifference <= 45) {
                 // Forward
-
+                recastData = new RecastData(FORWARD_RECAST_NAME);
+                recastDuration = SkillConfigManager.getUseSetting(hero, this, FORWARDS_RECAST_DURATION_NODE, DEFAULT_FORWARDS_RECAST_DURATION, false);
             } else if (yawDifference < 135) {
                 // Sideways
-
+                recastData = new RecastData(SIDEWAYS_RECAST_NAME);
+                recastDuration = SkillConfigManager.getUseSetting(hero, this, SIDEWAYS_RECAST_DURATION_NODE, DEFAULT_SIDEWAYS_RECAST_DURATION, false);
             } else {
                 // Backwards
-
+                recastData = new RecastData(BACKWARDS_RECAST_NAME);
+                recastDuration = SkillConfigManager.getUseSetting(hero, this, BACKWARDS_RECAST_DURATION_NODE, DEFAULT_BACKWARDS_RECAST_DURATION, false);
             }
+
+            if (recastDuration < MIN_RECAST_DURATION) {
+                recastDuration = MIN_RECAST_DURATION;
+            }
+            recastData.setReadyDelay(recastDuration);
 
             double yawMovementRadians = Math.toRadians(yawMovement + 90);
             Vector rollVelocity = new Vector(Math.cos(yawMovementRadians), 0.25, Math.sin(yawMovementRadians));
@@ -93,6 +135,31 @@ public class SkillTheySeeMeRolling extends ActiveSkill {
 
     @Override
     protected void recast(Hero hero, RecastData data) {
+        switch (data.getName()) {
+            case FORWARD_RECAST_NAME: {
+                forwardsRecast(hero);
+                break;
+            }
+            case SIDEWAYS_RECAST_NAME: {
+                sidewaysRecast(hero);
+                break;
+            }
+            case BACKWARDS_RECAST_NAME: {
+                backwardsRecast(hero);
+                break;
+            }
+        }
+    }
+
+    private void forwardsRecast(Hero hero) {
+
+    }
+
+    private void sidewaysRecast(Hero hero) {
+
+    }
+
+    private void backwardsRecast(Hero hero) {
 
     }
 }
