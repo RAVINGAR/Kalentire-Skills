737e77159ef22881c5c600bcdf4adf345d59f1d8
Werwew <werwewcode@gmail.com>
2019-01-29 16:21:39 -0500

    Fixed radius issue

--- src/com/herocraftonline/heroes/characters/skill/reborn/SkillRapidFire.java
+++ src/com/herocraftonline/heroes/characters/skill/reborn/SkillRapidFire.java
@@ -56,46 +56,55 @@ public class SkillRapidFire extends ActiveSkill {
 
     public SkillResult use(final Hero hero, String[] args) {
 
+        //Grab our hero casting our skill
         final Player player = hero.getPlayer();
 
-
-        int distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 10, false);
-        long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 5000, false);
-        long period = SkillConfigManager.getUseSetting(hero, this, SkillSetting.PERIOD, 500, false);
-
+        //Broadcast the hero is executing RapidFire
         broadcastExecuteText(hero);
 
+        //Distance of our Rapid Fire
+        int distance = SkillConfigManager.getUseSetting(hero, this, SkillSetting.MAX_DISTANCE, 10, false);
+
+        //Duration of our RapidFire
+        long duration = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DURATION, 5000, false);
+
+        //Period inbetween RapidFire shots
+        long period = SkillConfigManager.getUseSetting(hero, this, SkillSetting.PERIOD, 500, false);
+
+        //Damage of each RapidFire blast
         double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 90, false);
 
-
+        //Radius of our RapidFire. May or may not be configurable, revisit this.
         int radius = SkillConfigManager.getUseSetting(hero, this, SkillSetting.RADIUS, 2, false);
+
+        //Quick math method to square our radius.
         final int radiusSquared = radius * radius;
 
+        //Travel time? Might be useless we'll see.
         int delay = SkillConfigManager.getUseSetting(hero, this, "rapidfire-move-delay", 1, false);
 
 
-
-
-        iterate effect = new iterate(this, player, period, duration, distance, radiusSquared, delay, damage);
+        //Our method call.
+        RapidFireEffect effect = new RapidFireEffect(this, player, period, duration, distance, radiusSquared, delay, damage);
         hero.addEffect(effect);
 
         return SkillResult.NORMAL;
     }
 
 
+    //Method to keep the arrows firing straight for our visual.
     @EventHandler
     public void onProj(ProjectileLaunchEvent event) {
         if (event.getEntityType() != EntityType.ARROW)
             return;
-        if(!(event.getEntity() instanceof Arrow)) return;
+        if (!(event.getEntity() instanceof Arrow)) return;
         Arrow a = (Arrow) event.getEntity();
         Entity shooter = (Entity) event.getEntity().getShooter();
         a.setVelocity(shooter.getLocation().getDirection().multiply(2.0D));
     }
 
 
-
-    private class iterate extends PeriodicExpirableEffect {
+    private class RapidFireEffect extends PeriodicExpirableEffect {
 
         private final long _duration;
         private final int _distance;
@@ -105,7 +114,7 @@ public class SkillRapidFire extends ActiveSkill {
         private final double _radius;
 
 
-        public iterate(Skill skill, Player applier, long period, long duration, int distance, double radius, int delay, double damage) {
+        public RapidFireEffect(Skill skill, Player applier, long period, long duration, int distance, double radius, int delay, double damage) {
             super(skill, "iterate", applier, period, duration);
             _duration = duration;
             _distance = distance;
@@ -116,7 +125,6 @@ public class SkillRapidFire extends ActiveSkill {
             types.add(EffectType.PHYSICAL);
 
         }
-
 
 
         //Method to check if player is facing X to help radius accuracy.
@@ -141,6 +149,7 @@ public class SkillRapidFire extends ActiveSkill {
         @Override
         public void tickHero(Hero hero) {
 
+            //Cancel the ability if player gets stunned, disabled or silenced (should cover all the CC methods, if not add more to the loop).
             for (Effect effect : hero.getEffects()) {
                 if (effect.isType(EffectType.STUN) || effect.isType(EffectType.DISABLE) || effect.isType(EffectType.SILENCE)) {
                     hero.removeEffect(this);
@@ -148,9 +157,13 @@ public class SkillRapidFire extends ActiveSkill {
                 }
             }
 
+
             final Player player = hero.getPlayer();
 
             Block tempBlock;
+
+
+            //Define a BlockIterator which allows us to scale across blocks and define our damage effect.
             BlockIterator iter = null;
             try {
                 iter = new BlockIterator(player, _distance);
@@ -158,72 +171,85 @@ public class SkillRapidFire extends ActiveSkill {
                 e.printStackTrace();
             }
 
+            //Get all of our nearby entities in this defined are and add them to a list. Also create a list for enemies that were already hit.
             final List<Entity> nearbyEntities = player.getNearbyEntities(_radius, _radiusSquared, _radius);
             final List<Entity> hitEnemies = new ArrayList<>();
 
+            //Block Counter
             int numBlocks = 0;
+
+            //While our BlockIterator has more blocks to scan, lets keep running this loop.
             while (iter.hasNext()) {
+
+                //Our "tempBlock is constantly redefined by our Iterator loop
                 tempBlock = iter.next();
+                //Get the type of each tempBlock
                 Material tempBlockType = tempBlock.getType();
 
+
+                //If the iterator goes over transparent, blocks air ETC. continue and let's damage our target.
                 if (Util.transparentBlocks.contains(tempBlockType)) {
-                    final Location targetLocation = tempBlock.getLocation().clone().add(new Vector(.5, 0, .5));
+
+                    final Location targetLocation = tempBlock.getLocation().clone().add(new Vector(0.5, 0, 0.5));
 
                     Location loc = player.getLocation();
                     Vector x = loc.getDirection().normalize();
 
-                    //Arrow Visual
-                    Arrow shoot = player.getWorld().spawnArrow(targetLocation, x, (float) 0.5, (float) 1.0);
-                    shoot.setKnockbackStrength(0);
-                    shoot.setPickupStatus(Arrow.PickupStatus.DISALLOWED);
-                    shoot.setDamage(0);
-                    shoot.setGravity(false);
+//                    //Arrow Visual
+//                    Arrow shoot = player.getWorld().spawnArrow(targetLocation, x, (float) 0.5, (float) 1.0);
+//                    shoot.setKnockbackStrength(0);
+//                    shoot.setPickupStatus(Arrow.PickupStatus.DISALLOWED);
+//                    shoot.setDamage(0);
+//                    shoot.setGravity(false);
+//
+//                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+//                        public void run() {
+//                            shoot.remove();
+//                        }
+//                    }, 3L); //Edit ticks for arrows based on skill range, otherwise arrow visual will be thrown off****************
 
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                        public void run() {
-                            shoot.remove();
-                        }
-                    }, 3L); //Edit ticks for arrows based on skill range, otherwise arrow visual will be thrown off****************
+
+                    player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, targetLocation, 4, 0.3, 0.3, 0.3, 0.02);
+                    player.getWorld().spawnParticle(Particle.FLAME, targetLocation, 2, 0.3, 0.3, 0.3, 0.05);
+
+                    //targetLocation.getBlock().setType(Material.RED_STAINED_GLASS);
 
 
-                    // Schedule the action in advance
-                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                        public void run() {
-                            player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, targetLocation, 4, 0.3, 0.3, 0.3, 0.02);
-                            player.getWorld().spawnParticle(Particle.FLAME, targetLocation, 2, 0.3, 0.3, 0.3, 0.05);
+                    // Check our entity list to see if they are on this specific block at the moment the firework plays
+                    for (Entity entity : nearbyEntities) {
+                        // Ensure that we have a valid entity
 
-                            
+                        if (!(entity instanceof LivingEntity) || hitEnemies.contains(entity) || entity.getLocation().distanceSquared(targetLocation) > _radiusSquared)
+                            continue;
 
-                            // Check our entity list to see if they are on this specific block at the moment the firework plays
-                            for (Entity entity : nearbyEntities) {
-                                // Ensure that we have a valid entity
-                                if (!(entity instanceof LivingEntity) || hitEnemies.contains(entity) || entity.getLocation().distanceSquared(targetLocation) > _radiusSquared)
-                                    continue;
+                        // Check to see if the entity can be damaged
+                        if (!damageCheck(player, (LivingEntity) entity))
+                            continue;
 
-                                // Check to see if the entity can be damaged
-                                if (!damageCheck(player, (LivingEntity) entity))
-                                    continue;
+                        LivingEntity target = (LivingEntity) entity;
+                        addSpellTarget(target, hero);
 
+                        if (is_X_Direction(player)) {
                                 // Damage target
-                                LivingEntity target = (LivingEntity) entity;
-                                addSpellTarget(target, hero);
                                 damageEntity(target, player, _damage, EntityDamageEvent.DamageCause.PROJECTILE);
 
-
-                                // Add the target to the hitEntity map to ensure we don't ever hit them again with this specific BoneSpear
-                                hitEnemies.add(entity);
+                                addSpellTarget(target, hero);
+                                damageEntity(target, player, _damage, EntityDamageEvent.DamageCause.PROJECTILE);
                             }
+
+
+                            // Add the target to the hitEntity map to ensure we don't ever hit them again with this specific BoneSpear
+                            hitEnemies.add(entity);
+
                         }
-                    }, numBlocks * _delay);
-
-                    numBlocks++;
-
-                } else
+                    }
+                else
                     break;
+                }
             }
-
-
         }
     }
-}
+
+
+
 
