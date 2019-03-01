8fb19ba7d43ddb1538cf53091c3be7315c2cab6c
Donal Burke <dbrke98@gmail.com>
2019-03-01 01:53:23 +0000

    Testing passive skill for trident usage out of water

---
+++ src/com/herocraftonline/heroes/characters/skill/reborn/SkillTridentPassive.java
@@ -0,0 +1,90 @@
+package com.herocraftonline.heroes.characters.skill.reborn;
+
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.effects.EffectType;
+import com.herocraftonline.heroes.characters.skill.PassiveSkill;
+import com.herocraftonline.heroes.characters.skill.Skill;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import net.minecraft.server.v1_13_R2.EntityThrownTrident;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import com.herocraftonline.heroes.Heroes;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRiptideEvent;
+
+public class SkillTridentPassive extends PassiveSkill {
+
+    public SkillTridentPassive(Heroes plugin) {
+        super(plugin, "TridentPassive");
+        setDescription("You are able wield Tridents!");
+        setArgumentRange(0, 0);
+        setEffectTypes(EffectType.BENEFICIAL);
+        Bukkit.getServer().getPluginManager().registerEvents(new SkillTridentPassive.SkillListener(this), plugin);
+    }
+
+    @Override
+    public String getDescription(Hero hero) {
+        return getDescription();
+    }
+
+    @Override
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection config = super.getDefaultConfig();
+        config.set(SkillSetting.APPLY_TEXT.node(), "");
+        config.set(SkillSetting.UNAPPLY_TEXT.node(), "");
+        config.set(SkillSetting.LEVEL.node(), 1);
+        return config;
+    }
+
+    public class SkillListener implements Listener {
+
+        private final Skill skill;
+        public SkillListener(Skill skill) {
+            this.skill = skill;
+        }
+
+        // called when player right clicks trident, doesn't get called when they actually release the trident
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onPlayerInteract(PlayerInteractEvent event) {
+            if (event.getClickedBlock() == null || event.getClickedBlock().getType() != Material.TRIDENT || event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+                return;
+            }
+            Hero hero = plugin.getCharacterManager().getHero(event.getPlayer());
+            if (!hero.canUseSkill(skill)) {
+                event.setCancelled(true);
+                event.setUseInteractedBlock(Event.Result.DENY);
+            }
+
+            Player player = event.getPlayer();
+            player.sendMessage("InteractEvent!");
+        }
+
+        public void onPlayerLaunchProjectileEvent(ProjectileLaunchEvent event) {
+            Player player = (Player) event.getEntity().getShooter();
+            player.sendMessage("LaunchProjectile!");
+
+        }
+
+        public void onEntityThrowTrident(EntityThrownTrident event) {
+            Player player = (Player) event.getBukkitEntity();
+            player.sendMessage("EntityThrowTrident!");
+        }
+
+        // called when player launches trident with riptide enchantment
+        // not sure if it gets called when they are not in rain
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onPlayerRiptide(PlayerRiptideEvent event) {
+            Player player = event.getPlayer();
+            player.sendMessage("PlayerRiptide");
+        }
+
+    }
+}
