1fa5818b989833e65b73162ebb963b02044c2300
Delfofthebla <Delfofthebla@gmail.com>
2019-04-04 10:12:00 -0700

    Added delay to the hemorrhage skill.

--- src/com/herocraftonline/heroes/characters/skill/reborn/shared/SkillHemorrhage.java
+++ src/com/herocraftonline/heroes/characters/skill/reborn/shared/SkillHemorrhage.java
@@ -8,7 +8,6 @@ import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.characters.skill.SkillType;
 import com.herocraftonline.heroes.characters.skill.TargettedSkill;
-import com.herocraftonline.heroes.chat.ChatComponents;
 import com.herocraftonline.heroes.util.Util;
 import org.bukkit.Color;
 import org.bukkit.Particle;
@@ -18,12 +17,12 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
-
-import static org.bukkit.Color.*;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class SkillHemorrhage extends TargettedSkill {
 
-    //private static final Particle.DustOptions skillEffectDustOptions = new Particle.DustOptions(Color.RED, 1);
+    private static final DustOptions enterParticleOptions = new Particle.DustOptions(Color.RED, 1);
+    private static final DustOptions exitParticleOptions = new Particle.DustOptions(Color.RED, 2);
 
     public SkillHemorrhage(Heroes plugin) {
         super(plugin, "Hemorrhage");
@@ -49,7 +48,8 @@ public class SkillHemorrhage extends TargettedSkill {
         ConfigurationSection config = super.getDefaultConfig();
         config.set(SkillSetting.MAX_DISTANCE.node(), 3);
         config.set(SkillSetting.DAMAGE.node(), 30);
-        config.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), 0.7);
+        config.set(SkillSetting.DAMAGE_INCREASE_PER_STRENGTH.node(), 0.0);
+        config.set("ticks-before-damage", 3);
         return config;
     }
 
@@ -60,22 +60,35 @@ public class SkillHemorrhage extends TargettedSkill {
         broadcastExecuteText(hero, target);
 
         double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 30, false);
-        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, 0.7, false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_STRENGTH, 0.0, false);
         damage += damageIncrease * hero.getAttributeValue(AttributeType.STRENGTH);
 
-        // entrance of the hook
+        // display entrance of the hook
         player.getWorld().playSound(player.getLocation(), Sound.ENTITY_SQUID_HURT, 0.2F, 0.5F);
-        DustOptions hookEnter = new Particle.DustOptions(Color.RED, 1);
-        player.getWorld().spawnParticle(Particle.REDSTONE, target.getEyeLocation(),5, 0.5F, 0.25F, 0.3F, hookEnter);
+        player.getWorld().spawnParticle(Particle.REDSTONE, target.getEyeLocation(), 5, 0.5F, 0.25F, 0.3F, 1.5F, enterParticleOptions, false);
 
-        // do damage
-        addSpellTarget(target, hero);
-        damageEntity(target, player, damage, EntityDamageEvent.DamageCause.ENTITY_ATTACK);
+        int ticksBeforeDamage = SkillConfigManager.getUseSetting(hero, this, "ticks-before-damage", 3, false);
 
-        // removal of the hook
-        player.getWorld().playSound(player.getLocation(), Sound.ENTITY_SQUID_HURT, 0.4F, 1.0F);
-        DustOptions hookExit = new Particle.DustOptions(Color.RED, 2);
-        player.getWorld().spawnParticle(Particle.REDSTONE, target.getEyeLocation(),15, 0.25F, 0.15F, 0.4F, hookExit);
+        final double finalDamage = damage;  // Final for the runnable
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // Make sure that both player and target are still alive after our delay.
+                if (player.isDead() || player.getHealth() < 0)
+                    return;
+                if (target.isDead() || target.getHealth() < 0)
+                    return;
+
+                // do damage
+                addSpellTarget(target, hero);
+                damageEntity(target, player, finalDamage, EntityDamageEvent.DamageCause.ENTITY_ATTACK);
+
+                // display removal of the hook
+                player.getWorld().playSound(player.getLocation(), Sound.ENTITY_SQUID_HURT, 0.4F, 1.0F);
+                player.getWorld().spawnParticle(Particle.REDSTONE, target.getEyeLocation(), 15, 0.25F, 0.15F, 0.4F, 1.5F, exitParticleOptions, false);
+            }
+        }.runTaskLaterAsynchronously(plugin, ticksBeforeDamage);
+
         return SkillResult.NORMAL;
     }
 }
