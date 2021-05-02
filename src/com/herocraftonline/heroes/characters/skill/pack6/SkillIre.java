d95fb73889de135364a71d0686bda7ef887e178b
MysticMight <mysticmightdev@gmail.com>
2021-05-02 14:50:19 +1000

    add back Ire and update for 1.16.5 (was missing for some reason)

---
+++ src/com/herocraftonline/heroes/characters/skill/pack6/SkillIre.java
@@ -0,0 +1,66 @@
+package com.herocraftonline.heroes.characters.skill.pack6;
+// this skill was missing for some reason from this pack, maybe because I had to update its particle and sound? (and fully uncomment it)
+
+import org.bukkit.*;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import com.herocraftonline.heroes.Heroes;
+import com.herocraftonline.heroes.api.SkillResult;
+import com.herocraftonline.heroes.attributes.AttributeType;
+import com.herocraftonline.heroes.characters.Hero;
+import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
+import com.herocraftonline.heroes.characters.skill.SkillSetting;
+import com.herocraftonline.heroes.characters.skill.SkillType;
+import com.herocraftonline.heroes.characters.skill.TargettedSkill;
+
+public class SkillIre extends TargettedSkill {
+
+    public SkillIre(Heroes plugin) {
+        super(plugin, "Ire");
+        setDescription("You strike the target with nature's ire for $1 earth damage.");
+        setUsage("/skill ire");
+        setArgumentRange(0, 0);
+        setIdentifiers("skill ire");
+        setTypes(SkillType.ABILITY_PROPERTY_MAGICAL, SkillType.DAMAGING, SkillType.ABILITY_PROPERTY_EARTH, SkillType.SILENCEABLE, SkillType.AGGRESSIVE);
+    }
+
+    public String getDescription(Hero hero) {
+        int damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 80, false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 0.75, false);
+        damage += (int) (damageIncrease * hero.getAttributeValue(AttributeType.INTELLECT));
+
+        return getDescription().replace("$1", damage + "");
+    }
+
+    public ConfigurationSection getDefaultConfig() {
+        ConfigurationSection node = super.getDefaultConfig();
+
+        node.set(SkillSetting.MAX_DISTANCE.node(), 12);
+        node.set(SkillSetting.DAMAGE.node(), 80);
+        node.set(SkillSetting.DAMAGE_INCREASE_PER_INTELLECT.node(), 0.75);
+
+        return node;
+    }
+
+    public SkillResult use(Hero hero, LivingEntity target, String[] args) {
+        Player player = hero.getPlayer();
+
+        double damage = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE, 80, false);
+        double damageIncrease = SkillConfigManager.getUseSetting(hero, this, SkillSetting.DAMAGE_INCREASE_PER_INTELLECT, 0.75, false);
+        damage += (damageIncrease * hero.getAttributeValue(AttributeType.INTELLECT));
+
+        addSpellTarget(target, hero);
+        damageEntity(target, player, damage, DamageCause.MAGIC);
+
+        broadcastExecuteText(hero, target);
+
+        //target.getWorld().spigot().playEffect(target.getEyeLocation(), Effect.TILE_BREAK, org.bukkit.Material.DIRT.getId(), 0, 0.2F, 0.2F, 0.2F, 0.1F, 75, 16);
+        target.getWorld().spawnParticle(Particle.BLOCK_CRACK, target.getEyeLocation(),75,0.2F,0.2F,0.2F,0.1F, Bukkit.createBlockData(Material.DIRT));
+        target.getWorld().playSound(target.getLocation(), Sound.BLOCK_GRAVEL_HIT, 7.0F, 0.6F);
+
+        return SkillResult.NORMAL;
+    }
+}
