3019dfb9940c1313804e3765938751686c6c0ca7
RAVINGAR <ravingarinc@gmail.com>
2022-06-19 01:07:25 +0800

    Fix Craft Netherite for MMOItems

--- src/main/java/com/herocraftonline/heroes/characters/skill/general/SkillCraftNetheriteGear.java
+++ src/main/java/com/herocraftonline/heroes/characters/skill/general/SkillCraftNetheriteGear.java
@@ -8,6 +8,14 @@ import com.herocraftonline.heroes.characters.skill.SkillConfigManager;
 import com.herocraftonline.heroes.characters.skill.SkillSetting;
 import com.herocraftonline.heroes.util.MaterialUtil;
 import com.herocraftonline.heroes.util.Properties;
+import io.lumine.mythic.lib.api.item.NBTItem;
+import net.Indyuce.mmoitems.ItemStats;
+import net.Indyuce.mmoitems.MMOItems;
+import net.Indyuce.mmoitems.api.Type;
+import net.Indyuce.mmoitems.api.item.mmoitem.LiveMMOItem;
+import net.Indyuce.mmoitems.api.item.mmoitem.MMOItem;
+import net.Indyuce.mmoitems.stat.data.DoubleData;
+import net.Indyuce.mmoitems.stat.data.StringData;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -16,11 +24,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.inventory.PrepareSmithingEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Level;
 
 /**
@@ -82,10 +93,12 @@ public class SkillCraftNetheriteGear extends PassiveSkill {
     }
 
     public class SkillCraftingListener implements Listener {
+        private final Map<UUID, ItemStack> upgradables;
         private final PassiveSkill skill;
 
         public SkillCraftingListener(PassiveSkill skill) {
             this.skill = skill;
+            this.upgradables = new HashMap<>();
         }
 
         // Note Using the prepare event allows no result to appear when attempting to craft
@@ -113,13 +126,95 @@ public class SkillCraftNetheriteGear extends PassiveSkill {
                 hero.getPlayer().sendMessage(ChatColor.RED + "You must be level " + levelRequired + " to create "
                         + MaterialUtil.getFriendlyName(resultType) + "!");
                 event.setResult(null);
+                return;
                 //event.getInventory().setResult(null);
             }
 
-            //If we are at this point then craft is a success, replace with MMOItem todo
-            if(plugin.getServer().getPluginManager().getPlugin("MMOItems") != null) {
+            upgradables.put(event.getView().getPlayer().getUniqueId(), event.getInventory().getItem(0));
 
+            ItemStack hidden = new ItemStack(result.getType());
+            ItemMeta meta = hidden.getItemMeta();
+            meta.setDisplayName(ChatColor.RED + "???");
+            List<String> lore = new ArrayList<>();
+            lore.add(ChatColor.GRAY + "Unidentified item!");
+            lore.add(ChatColor.GRAY + "Upgrade this item to upgrade stats");
+            meta.setLore(lore);
+            hidden.setItemMeta(meta);
+            event.setResult(hidden);
+            //If we are at this point then craft is a success, replace with MMOItem todo
+        }
+
+        @EventHandler
+        public void onInventoryClose(InventoryCloseEvent event) {
+            if(event.getInventory().getType() == InventoryType.SMITHING) {
+                upgradables.remove(event.getPlayer().getUniqueId());
             }
+        }
+
+        @EventHandler
+        public void onInventoryClick(InventoryClickEvent event) {
+            //Reference https://paste.denizenscript.com/View/97562
+            if(event.isCancelled()) {
+                return;
+            }
+            if(event.getInventory().getType() == InventoryType.SMITHING && plugin.getServer().getPluginManager().getPlugin("MMOItems") != null) {
+                if(event.getSlotType() == InventoryType.SlotType.RESULT) {
+                    ItemStack original = upgradables.remove(event.getView().getPlayer().getUniqueId());
+
+                    Material material = Material.matchMaterial(original.getType().getKey().getKey().replaceAll("DIAMOND_", "NETHERITE_"));
+                    if(material != null) {
+                        original.setType(material);
+                    }
+                    else {
+                        event.setCancelled(true);
+                        return;
+                    }
+
+                    NBTItem nbtItem = NBTItem.get(original);
+                    if(nbtItem.hasType()) {
+                        Type type = MMOItems.plugin.getTypes().get(nbtItem.getType());
+                        String typeName = type.getName();
+                        MMOItem item = null;
+                        if(typeName.equalsIgnoreCase("SWORD") || typeName.equalsIgnoreCase("AXE") || typeName.equalsIgnoreCase("TOOL") || typeName.equalsIgnoreCase("SPEAR") || typeName.equalsIgnoreCase("STAFF")) {
+                            item =  upgradeWeaponMMOItem(nbtItem);
+                        }
+                        else if(typeName.equalsIgnoreCase("ARMOR")) {
+                            item = upgradeArmorMMOItem(nbtItem);
+                        }
+                        else {
+                            return;
+                        }
+
+                        NBTItem result = item.newBuilder().buildNBT();
+                        event.setCurrentItem(result.toItem());
+
+                    }
+                }
+            }
+        }
+
+        private MMOItem upgradeWeaponMMOItem(NBTItem item) {
+            MMOItem mmoItem = new LiveMMOItem(item);
+            mmoItem.setData(ItemStats.NAME, new StringData(mmoItem.getData(ItemStats.NAME).toString().replaceAll("Diamond", "Netherite")));
+            mmoItem.setData(ItemStats.MAX_DURABILITY, new DoubleData(Double.parseDouble(mmoItem.getData(ItemStats.MAX_DURABILITY).toString()) + 600));
+            mmoItem.setData(ItemStats.ATTACK_DAMAGE, new DoubleData(Double.parseDouble(mmoItem.getData(ItemStats.ATTACK_DAMAGE).toString()) + 2));
+            return mmoItem;
+        }
+
+        private MMOItem upgradeArmorMMOItem(NBTItem item) {
+            MMOItem mmoItem = new LiveMMOItem(item);
+
+            mmoItem.setData(ItemStats.NAME, new StringData(mmoItem.getData(ItemStats.NAME).toString().replaceAll("Diamond", "Netherite")));
+            mmoItem.setData(ItemStats.MAX_DURABILITY, new DoubleData(Double.parseDouble(mmoItem.getData(ItemStats.MAX_DURABILITY).toString()) + 1000));
+            //mmoItem.setData(ItemStats.ARMOR, new DoubleData(Double.parseDouble(mmoItem.getData(ItemStats.ARMOR).toString()) + 2));
+            mmoItem.setData(ItemStats.ARMOR_TOUGHNESS, new DoubleData(Double.parseDouble(mmoItem.getData(ItemStats.ARMOR_TOUGHNESS).toString()) + 1));
+            double base = 0.1;
+            if(mmoItem.hasData(ItemStats.KNOCKBACK_RESISTANCE)) {
+                base += Double.parseDouble(mmoItem.getData(ItemStats.KNOCKBACK_RESISTANCE).toString());
+            }
+            mmoItem.setData(ItemStats.KNOCKBACK_RESISTANCE, new DoubleData(base));
+
+            return mmoItem;
         }
     }
 //
